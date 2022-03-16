27eafbf0be306758ebc856b9058a423fd442355d
93Kamuran <enginkamuran@gmail.com>
2022-03-16 14:35:52 +0300

    migration files seperated.

---
+++ Website/htdocs/mpmanager/database/migrations/micropowermanager/2018_11_29_090603_create_clusters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clusters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('manager_id');
+            $table->foreign('manager_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->json('geo_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clusters');
+    }
+};
---
+++ Website/htdocs/mpmanager/database/migrations/micropowermanager/2020_10_29_170007_create_plugins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plugins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('composer_name');
+            $table->string('description');
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plugins');
+    }
+};
