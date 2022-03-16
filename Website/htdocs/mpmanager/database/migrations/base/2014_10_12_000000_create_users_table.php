e99d73e9851d2b454a192f013fcb8aa1155f69af
93Kamuran <enginkamuran@gmail.com>
2022-03-16 14:41:45 +0300

    company_id column added to users table.

--- Website/htdocs/mpmanager/database/migrations/base/2014_10_12_000000_create_users_table.php
+++ Website/htdocs/mpmanager/database/migrations/base/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('company_id')->unsigned();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
