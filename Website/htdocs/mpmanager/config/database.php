71b6f56dfb49db561083dbe2c58723158bf0804d
InensusDev <developer@inensus.com>
2022-03-30 18:06:54 +0300

    database configurations refactored for testing.

--- Website/htdocs/mpmanager/config/database.php
+++ Website/htdocs/mpmanager/config/database.php
@@ -32,25 +32,19 @@ return [
     */
 
     'connections' => [
-        'companyA_1648543301' => [ 
-            'driver' => 'mysql', 
-            'host' => 'db', 
-            'port' => '3306', 
-            'database' => 'companyA_1648543301', 
-            'username' => 'root', 
-            'password' => 'inensus2022.', 
-            'unix_socket' => '', 
-            'charset' => 'utf8mb4', 
-            'collation' => 'utf8mb4_unicode_ci', 
-            'prefix' => '', 
-            'strict' => true, 
-            'engine' => null, 
-        ],
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        'test_company_db' => [
+            'driver' => 'mysql',
+            'host' => env('TEST_DB_HOST', '127.0.0.1'),
+            'port' => env('TEST_DB_PORT', '3306'),
+            'database' => env('TEST_DB_DATABASE', 'forge'),
+            'username' => env('TEST_DB_USERNAME', 'forge'),
+            'password' => env('TEST_DB_PASSWORD', ''),
+            'unix_socket' => env('TEST_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
 
         'micro_power_manager' => [
@@ -68,30 +62,19 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+        // PHPunit testing main connection
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
             'prefix' => '',
         ],
 
+        // PHPunit testing test_db connection
+        'testing_test_company_db' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]
     ],
 
     /*
