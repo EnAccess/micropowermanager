c806a5e6be7a7e5b4a1990db67ff9865dba59367
InensusDev <developer@inensus.com>
2022-04-18 14:38:19 +0300

    MeterParameterFactory created.

---
+++ Website/htdocs/mpmanager/database/factories/MeterParameterFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Meter\MeterParameter;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MeterParameterFactory extends Factory
+{
+    protected $model = MeterParameter::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'owner_type'=>'person',
+            'owner_id'=>$this->faker->randomNumber(1),
+            'meter_id'=>$this->faker->randomNumber(1),
+            'tariff_id'=>$this->faker->randomNumber(1),
+            'connection_type_id'=>$this->faker->randomNumber(1),
+            'connection_group_id'=>$this->faker->randomNumber(1),
+        ];
+    }
+}
