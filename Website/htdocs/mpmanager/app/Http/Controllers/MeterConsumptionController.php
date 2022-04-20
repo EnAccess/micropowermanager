605d9baf6d19d0174b7eb584eea098dbfbea59ba
InensusDev <developer@inensus.com>
2022-04-20 13:57:21 +0300

    Created meter related controllers.

---
+++ Website/htdocs/mpmanager/app/Http/Controllers/MeterConsumptionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\MeterConsumptionService;
+use App\Models\Meter\MeterConsumption;
+use App\Services\MeterService;
+
+class MeterConsumptionController extends Controller
+{
+    public function __construct(
+        private MeterService $meterService,
+        private MeterConsumptionService $meterConsumptionService
+    ) {
+    }
+
+    /**
+     * Consumption List
+     * If the meter has the ability to send data to your server. That is the endpoint where you get the
+     * meter readings ( used energy, credit on meter etc.)
+     *
+     * @urlParam     serialNumber
+     * @urlParam     start YYYY-mm-dd format
+     * @urlParam     end YYYY-mm-dd format
+     * @responseFile responses/meters/meter.consumption.list.json
+     *
+     * @param  $serialNumber
+     * @param  $start
+     * @param  $end
+     * @return ApiResource
+     */
+    public function show($serialNumber, $start, $end): ApiResource
+    {
+        $meter = $this->meterService->getBySerialNumber($serialNumber);
+
+        return ApiResource::make($this->meterConsumptionService->getByMeter($meter, $start, $end));
+    }
+}
---
+++ Website/htdocs/mpmanager/app/Http/Controllers/PersonMeterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\PersonMeterService;
+
+class PersonMeterController
+{
+
+    public function __construct(
+        private PersonMeterService $personMeterService
+    ) {
+
+    }
+
+    /**
+     * @group    People
+     * Person with Meters & Tariff
+     * Person details with his/her owned meter(s) and its assigned tariff
+     * @param int $personId
+     * @urlParam person required The ID of the person
+     * @return   ApiResource
+     *
+     * @responseFile responses/people/person.meter.tariff.json
+     */
+    public function show(int $personId): ApiResource
+    {
+        return  ApiResource::make($this->personMeterService->getPersonMeters($personId));
+    }
+}
