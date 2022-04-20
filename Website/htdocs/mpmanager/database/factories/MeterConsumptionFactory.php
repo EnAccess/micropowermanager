bcf863424db96eab8dcb1cd551e9b0b95abf7eca
InensusDev <developer@inensus.com>
2022-04-20 13:56:26 +0300

    Created necessary factory classes.

---
+++ Website/htdocs/mpmanager/database/factories/MeterConsumptionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Meter\MeterConsumption;
+
+class MeterConsumptionFactory extends Factory
+{
+    protected $model = MeterConsumption::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'meter_id'=>1,
+            'total_consumption'=>$this->faker->randomFloat(2,0,100),
+            'consumption'=>$this->faker->randomFloat(2,0,10),
+            'credit_on_meter'=>$this->faker->randomFloat(2,0,5),
+            'reading_date'=>Carbon::now()->subDays(1),
+
+        ];
+    }
+}
---
+++ Website/htdocs/mpmanager/database/factories/MeterTokenFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Meter\MeterToken;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MeterTokenFactory extends Factory
+{
+
+    protected $model = MeterToken::class;
+
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'transaction_id' => 1,
+            'meter_id' => 1,
+            'token' => '123456789',
+            'energy' => 0.123
+        ];
+    }
+}
