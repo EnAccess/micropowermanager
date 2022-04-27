640061ea08558ee1ba8c755ca27f6e97712e24fa
InensusDev <developer@inensus.com>
2022-04-27 15:56:03 +0300

    Created cluster & cluster revenue related controllers.

---
+++ Website/htdocs/mpmanager/app/Http/Controllers/ClusterMiniGridRevenueController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\ClusterRevenueService;
+use App\Services\ClusterService;
+use App\Services\ConnectionTypeService;
+use App\Services\MeterRevenueService;
+use Illuminate\Http\Request;
+
+class ClusterMiniGridRevenueController extends Controller
+{
+    public function __construct(
+        private ClusterRevenueService $clusterRevenueService
+    ) {
+    }
+
+    public function show($clusterId, Request $request): ApiResource
+    {
+        $startDate = $request->input('startDate') ?? date('Y-01-01');
+        $endDate = $request->input('endDate') ?? date('Y-m-t');
+        $period = $request->input('period') ?? 'monthly';
+
+        return ApiResource::make($this->clusterRevenueService->getMiniGridBasedRevenueById($clusterId,
+            $startDate, $endDate, $period));
+    }
+}
---
+++ Website/htdocs/mpmanager/app/Http/Controllers/ClusterRevenueController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\ClusterMeterService;
+use App\Services\ClusterMiniGridService;
+use App\Services\ClusterPopulationService;
+use App\Services\ClusterRevenueService;
+use App\Services\ClusterService;
+use App\Services\ClusterTransactionService;
+use App\Services\MeterRevenueService;
+use App\Services\MeterService;
+use App\Services\PeriodService;
+use Illuminate\Http\Request;
+
+class ClusterRevenueController extends Controller
+{
+    public function __construct(
+        private ClusterService $clusterService,
+        private ClusterRevenueService $clusterRevenueService,
+        private ClusterMiniGridService $clusterMiniGridService,
+        private ClusterMeterService $clusterMeterService,
+        private ClusterTransactionService $clusterTransactionService,
+        private ClusterPopulationService $clusterPopulationService,
+        private PeriodService $periodService
+    ) {
+    }
+
+    public function index(Request $request): ApiResource
+    {
+        $dates =
+            $this->clusterRevenueService->setDatesForRequest($request->get('startDate'), $request->get('endDate'));
+        $startDate = $dates['startDate'];
+        $endDate = $dates['endDate'];
+        $period = $request->get('period') ?? 'monthly';
+        $clusters = $this->clusterMiniGridService->getClustersWithMiniGrids();
+        $periods = $this->periodService->generatePeriodicList($startDate, $endDate, $period, ['revenue' => 0]);
+
+        return ApiResource::make(
+            $this->clusterRevenueService->getPeriodicRevenueForClusters($clusters, $startDate, $endDate, $periods,
+                $period));
+
+    }
+
+    public function show($clusterId, Request $request): ApiResource
+    {
+        $dateRange =
+            $this->clusterRevenueService->setDateRangeForRequest($request->get('startDate'), $request->get('endDate'));
+        $cluster = $this->clusterService->getById($clusterId);
+        $cluster->meterCount = $this->clusterMeterService->getCountById($cluster->id);
+        $cluster->revenue = $this->clusterTransactionService->getById($cluster->id, $dateRange);
+        $cluster->population = $this->clusterPopulationService->getById($cluster->id);
+
+        return ApiResource::make($cluster);
+    }
+}
