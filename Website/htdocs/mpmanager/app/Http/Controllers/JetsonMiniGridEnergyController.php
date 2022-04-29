8d2a4f738892e3165a3c90a88c15262195fcd04a
InensusDev <developer@inensus.com>
2022-04-29 13:01:12 +0300

    Created MiniGrid & MiniGrid  revenue related controllers.

---
+++ Website/htdocs/mpmanager/app/Http/Controllers/JetsonMiniGridEnergyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+use App\Services\MiniGridEnergyService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class JetsonMiniGridEnergyController extends Controller
+{
+
+   public function __construct(private MiniGridEnergyService $miniGridEnergyService)
+   {
+   }
+
+    public function show($miniGridId,Request $request): ApiResource
+    {
+        $startDate = $request->input('start_date');
+        $endDate = $request->input('end_date');
+
+        return  ApiResource::make($this->miniGridEnergyService->getById($miniGridId,$startDate,$endDate));
+    }
+}
---
+++ Website/htdocs/mpmanager/app/Http/Controllers/MiniGridRevenueController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ApiResource;
+
+use App\Services\MeterRevenueService;
+use App\Services\MeterService;
+use App\Services\MiniGridRevenueService;
+use Illuminate\Http\Request;
+
+class MiniGridRevenueController
+{
+    public function __construct(
+        private MeterService $meterService,
+        private MiniGridRevenueService $miniGridRevenueService
+    ) {
+    }
+
+    public function show($miniGridId, Request $request): ApiResource
+    {
+        $startDate = $request->get('startDate');
+        $endDate = $request->get('endDate');
+
+        if (preg_match('/\/energy/', $request->url())){
+
+            return ApiResource::make($this->miniGridRevenueService->getSoldEnergyById($miniGridId, $startDate, $endDate,
+                $this->meterService));;
+        }
+
+        return ApiResource::make($this->miniGridRevenueService->getById($miniGridId, $startDate, $endDate,
+            $this->meterService));
+    }
+
+}
