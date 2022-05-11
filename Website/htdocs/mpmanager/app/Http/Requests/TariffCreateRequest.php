8cce7cf5fb8ddb62ac8c51d568ca397d5354cc92
InensusDev <developer@inensus.com>
2022-05-11 17:40:53 +0300

    Refactored TariffCreateRequest.

--- Website/htdocs/mpmanager/app/Http/Requests/TariffCreateRequest.php
+++ Website/htdocs/mpmanager/app/Http/Requests/TariffCreateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Services\SessionService;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\JsonResponse;
@@ -27,25 +28,28 @@ class TariffCreateRequest extends FormRequest
      */
     public function rules()
     {
+        $sessionService = app()->make(SessionService::class);
+        $database=$sessionService->getAuthenticatedUserDatabaseName();
+
         return [
-            'name' => ['required',Rule::unique('meter_tariffs')->ignore($this->id)],
+            'name' => ['required',Rule::unique($database.'.meter_tariffs')->ignore($this->id)],
             'price' => 'required|integer', // 100 times of original price to support 2 decimal numbers.
             'currency' => 'required|string|max:20',
             'factor' => 'sometimes|integer',
-            'access_rate_period' => 'required_with:access_rate_amount|integer|min:1',
-            'access_rate_amount' => 'required_with:access_rate_period|integer',
+            'access_rate_period' => 'required_with:'.$database.'.access_rate_amount|integer|min:1',
+            'access_rate_amount' => 'required_with:'.$database.'.access_rate_period|integer',
             'social_tariff' => 'sometimes|required',
-            'social_tariff.daily_allowance' => 'required_with:social_tariff',
-            'social_tariff.price' => 'required_with:social_tariff',
-            'social_tariff.initial_energy_budget' => 'required_with:social_tariff',
-            'social_tariff.maximum_stacked_energy' => 'required_with:social_tariff',
+            'social_tariff.daily_allowance' => 'required_with:'.$database.'.social_tariff',
+            'social_tariff.price' => 'required_with:'.$database.'.social_tariff',
+            'social_tariff.initial_energy_budget' => 'required_with:'.$database.'.social_tariff',
+            'social_tariff.maximum_stacked_energy' => 'required_with:'.$database.'.social_tariff',
             'components' => 'sometimes|required|array',
-            'components.*.name' => 'required_with:components',
-            'components.*.price' => 'required_with:components',
+            'components.*.name' => 'required_with:'.$database.'.components',
+            'components.*.price' => 'required_with:'.$database.'.components',
             'tous' => 'sometimes|required|array',
-            'tous.*.start' => 'required_with:tous',
-            'tous.*.end' => 'required_with:tous',
-            'tous.*.value' => 'required_with:tous',
+            'tous.*.start' => 'required_with:'.$database.'.tous',
+            'tous.*.end' => 'required_with:'.$database.'.tous',
+            'tous.*.value' => 'required_with:'.$database.'.tous',
         ];
     }
 
