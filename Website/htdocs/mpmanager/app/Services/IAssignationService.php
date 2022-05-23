ec93f71f3ced7ae88388eb15151cadee3a10d58b
InensusDev <developer@inensus.com>
2022-05-23 11:02:04 +0300

    Created IAssociative & IAssignationService interfaces for specifying poly morphic relations in service layer.

---
+++ Website/htdocs/mpmanager/app/Services/IAssignationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+
+
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Model;
+
+interface IAssignationService
+{
+    public function setAssigned($assigned);
+
+    public function setAssigner($assigning);
+
+    public function assign();
+}
