61c8c1c345afa092bdcd5206b36adf6acc2481e3
Alchalade <alchalade@gmail.com>
2022-09-12 20:24:01 +0300

    (jobs) drop not needed code.

--- Website/htdocs/mpmanager/app/Jobs/CreatePiggyBankEntry.php
+++ Website/htdocs/mpmanager/app/Jobs/CreatePiggyBankEntry.php
@@ -3,15 +3,10 @@
 namespace App\Jobs;
 
 use App\Models\Meter\MeterParameter;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+
 
 class CreatePiggyBankEntry extends AbstractJob
 {
-
 
     public function __construct(private MeterParameter $meterParameter)
     {
@@ -19,7 +14,6 @@ class CreatePiggyBankEntry extends AbstractJob
 
     public function handle()
     {
-        echo "create piggy bank " . $this->meterParameter->tariff->id . "\n";
         if ($socialTariff = $this->meterParameter->tariff()->first()->socialTariff) {
             $this->meterParameter->socialTariffPiggyBank()->create(
                 [
