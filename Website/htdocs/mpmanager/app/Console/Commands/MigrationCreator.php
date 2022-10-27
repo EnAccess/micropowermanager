4b4b8549533dd07a9246b06190d467d577b082ae
InensusDev <developer@inensus.com>
2022-10-27 16:36:25 +0300

    Refactored. - It was giving __invoke() does not exist error. - Also this command for creating recently created migrations on /migrations/micropowermanager directory

--- Website/htdocs/mpmanager/app/Console/Commands/MigrationCreator.php
+++ Website/htdocs/mpmanager/app/Console/Commands/MigrationCreator.php
@@ -5,17 +5,46 @@ namespace App\Console\Commands;
 use App\Models\CompanyDatabase;
 use Illuminate\Console\Command;
 
-class MigrationCreator extends AbstractSharedCommand
+class MigrationCreator extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'migrator:create {migration-name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'creates new migrations into micropowermanager direction.';
 
-    public function runInCompanyScope(): void
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
     {
         $migrationName = $this->argument('migration-name');
         $this->call('make:migration', [
             'name' => $migrationName,
             '--path' => '/database/migrations/micropowermanager'
         ]);
+
+
+        return 0;
     }
 }
