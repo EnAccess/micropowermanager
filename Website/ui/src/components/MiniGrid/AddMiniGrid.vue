6dfd5f94723f197317ad680caf86fb0bbfe7df64
InensusDev <developer@inensus.com>
2022-10-27 14:40:14 +0300

    Fixed.

--- Website/ui/src/components/MiniGrid/AddMiniGrid.vue
+++ Website/ui/src/components/MiniGrid/AddMiniGrid.vue
@@ -3,25 +3,25 @@
         <widget
             :title="$tc('phrases.newMiniGrid')"
             color="green"
-            >
+        >
             <md-card>
                 <md-card-content>
                     <div class="md-layout md-gutter md-size-100">
                         <div class="md-layout-item md-size-70 md-small-size-100">
                             <md-field :class="{'md-invalid': errors.has($tc('words.name'))}">
-                                <label for="miniGrid_name">{{$tc('words.name')}}</label>
+                                <label for="miniGrid_name">{{ $tc('words.name') }}</label>
                                 <md-input
                                     id="miniGridName"
                                     :name="$tc('words.name')"
                                     v-model="miniGridName"
                                     v-validate="'required|min:3'"></md-input>
-                                <span class="md-error">{{errors.first($tc('words.name'))}}</span>
+                                <span class="md-error">{{ errors.first($tc('words.name')) }}</span>
                             </md-field>
 
                         </div>
                         <div class="md-layout-item md-size-30 md-small-size-100">
                             <md-field :class="{'md-invalid': errors.has($tc('words.cluster'))}">
-                                <label for="clusterName">{{$tc('words.cluster')}}</label>
+                                <label for="clusterName">{{ $tc('words.cluster') }}</label>
                                 <md-select
                                     v-model="selectedClusterId"
                                     :name="$tc('words.cluster')"
@@ -30,22 +30,23 @@
                                     @md-selected="selectCluster">
                                     <md-option v-for="(cluster) in clusters" :value="cluster.id"
                                                :key="cluster.id">
-                                        {{cluster.name}}
+                                        {{ cluster.name }}
                                     </md-option>
                                 </md-select>
-                                <span class="md-error">{{errors.first($tc('words.cluster'))}}</span>
+                                <span class="md-error">{{ errors.first($tc('words.cluster')) }}</span>
                             </md-field>
                         </div>
                     </div>
                     <div class="md-layout md-gutter md-size-100">
-                        <div class="md-layout md-gutter md-size-60 md-small-size-100" style="padding-left: 1.5rem !important;">
+                        <div class="md-layout md-gutter md-size-60 md-small-size-100"
+                             style="padding-left: 1.5rem !important;">
                             <form class="md-layout md-gutter" @submit.prevent="validatePoints"
-                                   style="padding-left: 1.5rem !important;"
+                                  style="padding-left: 1.5rem !important;"
                             >
 
                                 <div class="md-layout-item md-size-30 md-small-size-100">
                                     <md-field :class="{'md-invalid': errors.has($tc('words.latitude'))}">
-                                        <label for="latitude">{{$tc('words.latitude')}}</label>
+                                        <label for="latitude">{{ $tc('words.latitude') }}</label>
                                         <md-input
                                             id="latitude"
                                             :name="$tc('words.latitude')"
@@ -53,12 +54,12 @@
                                             step="any"
                                             v-model="miniGridLatLng.lat"
                                             v-validate="'required|decimal:5|max:8'"></md-input>
-                                        <span class="md-error">{{errors.first($tc('words.latitude'))}}</span>
+                                        <span class="md-error">{{ errors.first($tc('words.latitude')) }}</span>
                                     </md-field>
                                 </div>
                                 <div class="md-layout-item md-size-30 md-small-size-100">
                                     <md-field :class="{'md-invalid': errors.has($tc('words.longitude'))}">
-                                        <label for="longitude">{{$tc('words.longitude')}}</label>
+                                        <label for="longitude">{{ $tc('words.longitude') }}</label>
                                         <md-input
                                             id="longitude"
                                             :name="$tc('words.longitude')"
@@ -67,11 +68,13 @@
                                             maxlength="8"
                                             v-model="miniGridLatLng.lon"
                                             v-validate="'required|decimal:5|max:8'"></md-input>
-                                        <span class="md-error">{{errors.first($tc('words.longitude'))}}</span>
+                                        <span class="md-error">{{ errors.first($tc('words.longitude')) }}</span>
                                     </md-field>
                                 </div>
                                 <div class="md-layout-item md-size-40">
-                                    <md-button type="submit" class="md-primary set-button">{{$tc('phrases.setPoints')}}</md-button>
+                                    <md-button type="submit" class="md-primary set-button">
+                                        {{ $tc('phrases.setPoints') }}
+                                    </md-button>
 
                                 </div>
                             </form>
@@ -79,7 +82,8 @@
                         </div>
 
                         <div class="md-layout-item md-size-40 md-small-size-100">
-                            <md-button class="md-primary save-button" @click="saveMiniGrid()">{{$tc('words.save')}}</md-button>
+                            <md-button class="md-primary save-button" @click="saveMiniGrid()">{{ $tc('words.save') }}
+                            </md-button>
 
                         </div>
                     </div>
@@ -129,7 +133,7 @@ export default {
             miniGridService: new MiniGridService(),
             clusterId: null,
             geoData: null,
-            center: [this.$store.getters['settings/getMapSettings'].latitude,this.$store.getters['settings/getMapSettings'].longitude],
+            center: [this.$store.getters['settings/getMapSettings'].latitude, this.$store.getters['settings/getMapSettings'].longitude],
             marker: true,
             markerUrl: marker,
             miniGridName: '',
@@ -201,9 +205,9 @@ export default {
                 try {
                     this.loading = true
                     const miniGrid = await this.miniGridService.createMiniGrid(this.miniGridName, this.clusterId, this.miniGridLatLng)
-                    this.alertNotify('success', this.$tc('phrases.newMiniGrid',2))
+                    this.alertNotify('success', this.$tc('phrases.newMiniGrid', 2))
                     this.loading = false
-                    await this.$router.replace('/dashboards/mini-grid/' + miniGrid.id)
+                    await this.$router.replace('/locations/add-village?id=' + miniGrid.id)
                 } catch (e) {
                     this.loading = false
                     this.alertNotify('error', e.message)
@@ -235,29 +239,29 @@ export default {
 </script>
 
 <style scoped>
-    .md-progress-bar {
-        position: absolute;
-        top: 0;
-        right: 0;
-        left: 0;
-    }
+.md-progress-bar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+}
 
-    .save-button {
-        background-color: #325932 !important;
-        color: #fefefe !important;
-        top: 0.5rem;
-        float: right;
-    }
+.save-button {
+    background-color: #325932 !important;
+    color: #fefefe !important;
+    top: 0.5rem;
+    float: right;
+}
 
-    .set-button {
-        background-color: #448aff !important;
-        color: #fefefe !important;
-        top: 0.5rem;
-        float: left;
-    }
+.set-button {
+    background-color: #448aff !important;
+    color: #fefefe !important;
+    top: 0.5rem;
+    float: left;
+}
 
-    .map-area {
-        z-index: 1 !important
-    }
+.map-area {
+    z-index: 1 !important
+}
 
 </style>
