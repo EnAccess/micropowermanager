fb21ccc6396fd5ad73d80d640e8ef27101b6a2f8
InensusDev <developer@inensus.com>
2022-11-08 19:08:04 +0300

    Viber messaging plugin UI

---
+++ Website/ui/src/plugins/viber-messaging/js/routes.js
@@ -0,0 +1,7 @@
+let routes = [
+    {
+        path: '/viber-messaging/viber-overview',
+        component: require('./plugins/viber-messaging/js/components/Overview/Overview').default,
+        meta: { layout: 'default' },
+    }
+]
