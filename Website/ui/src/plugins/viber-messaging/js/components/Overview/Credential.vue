792002ba22154e9f920585440537b02159261e96
InensusDev <developer@inensus.com>
2022-11-09 17:08:49 +0300

    UI fix.

--- Website/ui/src/plugins/viber-messaging/js/components/Overview/Credential.vue
+++ Website/ui/src/plugins/viber-messaging/js/components/Overview/Credential.vue
@@ -25,8 +25,10 @@
 
                                 <div v-if="credentialService.credential.deepLink"
                                      class="md-layout-item  md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100">
-                                    <p> Please distribute {{ credentialService.credential.deepLink }} to your customers
-                                        and
+                                    <p> Please distribute
+                                        <br>
+                                        <span style="font-weight: bold"> {{ credentialService.credential.deepLink }}</span>
+                                        <br> to your customers and
                                         ask them to send their meter number to register our Viber communication
                                         system. </p>
                                 </div>
