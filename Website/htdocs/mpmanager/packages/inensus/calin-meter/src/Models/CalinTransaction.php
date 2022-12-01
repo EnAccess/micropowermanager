676b6ccac9dba387edf2a583ef28a0ad7fc65dcb
InensusDev <developer@inensus.com>
2022-12-01 13:21:40 +0300

    refactored

--- Website/htdocs/mpmanager/packages/inensus/calin-meter/src/Models/CalinTransaction.php
+++ Website/htdocs/mpmanager/packages/inensus/calin-meter/src/Models/CalinTransaction.php
@@ -3,8 +3,50 @@
 namespace Inensus\CalinMeter\Models;
 
 use App\Models\BaseModel;
+use App\Models\Transaction\AgentTransaction;
+use App\Models\Transaction\AirtelTransaction;
+use App\Models\Transaction\ISubTransaction;
+use App\Models\Transaction\ThirdPartyTransaction;
+use App\Transaction\VodacomTransaction;
+use Inensus\MesombPaymentProvider\Models\MesombTransaction;
+use Inensus\SwiftaPaymentProvider\Models\SwiftaTransaction;
+use Inensus\WaveMoneyPaymentProvider\Models\WaveMoneyTransaction;
 
-class CalinTransaction extends BaseModel
+class CalinTransaction extends BaseModel implements ISubTransaction
 {
     protected $table = 'calin_transactions';
+
+    public function agentTransaction()
+    {
+        return $this->morphOne(AgentTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function vodacomTransaction()
+    {
+        return $this->morphOne(VodacomTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function airtelTransaction()
+    {
+        return $this->morphOne(AirtelTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function thirdPartyTransaction()
+    {
+        return $this->morphOne(ThirdPartyTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function mesombTransaction()
+    {
+        return $this->morphOne(MesombTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function swiftaTransaction()
+    {
+        return $this->morphOne(SwiftaTransaction::class, 'manufacturer_transaction');
+    }
+    public function waveMoneyTransaction()
+    {
+        return $this->morphOne(WaveMoneyTransaction::class, 'manufacturer_transaction');
+    }
 }
--- Website/htdocs/mpmanager/packages/inensus/steama-meter/src/Models/SteamaTransaction.php
+++ Website/htdocs/mpmanager/packages/inensus/steama-meter/src/Models/SteamaTransaction.php
@@ -8,21 +8,20 @@ use App\Models\Transaction\AgentTransaction;
 use App\Models\Transaction\AirtelTransaction;
 use App\Transaction\VodacomTransaction;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Inensus\MesombPaymentProvider\Models\MesombTransaction;
+use Inensus\SwiftaPaymentProvider\Models\SwiftaTransaction;
+use Inensus\WaveMoneyPaymentProvider\Models\WaveMoneyTransaction;
 
 class SteamaTransaction extends BaseModel implements ISubTransaction
 {
     protected $table = 'steama_transactions';
 
 
-    public function category()
-    {
-        return $this->belongsTo(SteamaTransactionCategory::class, 'category_id');
-    }
     public function site()
     {
         return $this->belongsTo(SteamaSite::class, 'site_id', 'site_id');
     }
-    public function agentTransaction(): MorphOne
+    public function agentTransaction()
     {
         return $this->morphOne(AgentTransaction::class, 'manufacturer_transaction');
     }
@@ -40,5 +39,19 @@ class SteamaTransaction extends BaseModel implements ISubTransaction
     public function thirdPartyTransaction()
     {
         return $this->morphOne(ThirdPartyTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function mesombTransaction()
+    {
+        return $this->morphOne(MesombTransaction::class, 'manufacturer_transaction');
+    }
+
+    public function swiftaTransaction()
+    {
+        return $this->morphOne(SwiftaTransaction::class, 'manufacturer_transaction');
+    }
+    public function waveMoneyTransaction()
+    {
+        return $this->morphOne(WaveMoneyTransaction::class, 'manufacturer_transaction');
     }
 }
