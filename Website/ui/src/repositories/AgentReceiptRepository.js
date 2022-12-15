dbbe9b9a7dba584a1f2a3c268088ced7c9c1c552
InensusDev <developer@inensus.com>
2022-12-16 02:25:43 +0300

    bug fixing

--- Website/ui/src/repositories/AgentReceiptRepository.js
+++ Website/ui/src/repositories/AgentReceiptRepository.js
@@ -9,8 +9,8 @@ export default {
         return Client.get(`${resource}/${agentId}`)
 
     },
-    create(newReceipt, agentId){
-        return Client.post(`${resource}/${agentId}`,newReceipt)
+    create(newReceipt){
+        return Client.post(`${resource}`,newReceipt)
 
     }
 }
