728e30080b7b2f7b277cee4511c1b13ae136685b
Inensus Inensus <developer@inensus.com>
2023-01-24 10:01:44 +0000

    Update register_welcome.blade.php

--- Website/htdocs/mpmanager/resources/views/templates/mail/register_welcome.blade.php
+++ Website/htdocs/mpmanager/resources/views/templates/mail/register_welcome.blade.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 Dear <strong>{{$userName}}</strong> from {{$companyName}},<br> thank you for registering. There are plenty of plugins that could help to optimize your business.
-Pleae checkout https://micropowermanager.com/. Here is also an introduction video of MPM: https://www.youtube.com/watch?v=egD0jV3b4dY
+Please checkout https://micropowermanager.com/. Here is also an introduction video of MPM: https://www.youtube.com/watch?v=egD0jV3b4dY
 
 This is an automatic mail please don't reply to it.
 
