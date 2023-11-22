29e870a2b53703305ccec93992963d05a3327a24
Daniel Mohns <dmohns@users.noreply.github.com>
2023-11-22 13:18:41 +0100

    Remove internal api code (#5)
    * Remove `InternalApi`
    
    * Add `InternalApi` to `.gitignore`

--- InternalApi/company/22/miniGrid/1/forecast-tool/test.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import time
-
-
-time.sleep(15)
-print("Output from Python")
-print("First name: " + sys.argv[1])
-print("Last name: " + sys.argv[2])
--- InternalApi/template/forecast-tool/README.md
+++
@@ -1,2 +0,0 @@
-# Forecast Tool V2
-
