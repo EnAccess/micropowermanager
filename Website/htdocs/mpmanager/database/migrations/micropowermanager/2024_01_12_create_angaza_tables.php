ebf282655ebc4a90a948cc8be2115afd7aae9c1d
InensusDev <developer@inensus.com>
2024-01-18 17:13:37 +0300

    added forgotten file

---
+++ Website/htdocs/mpmanager/database/migrations/micropowermanager/2024_01_12_create_angaza_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        if (!Schema::connection('shard')->hasTable('angaza_api_credentials')) {
+            Schema::connection('shard')->create('angaza_api_credentials', static function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('api_url')->default('https://payg.angazadesign.com/nexus/v1');
+                $table->string('client_id')->nullable();
+                $table->string('client_secret')->nullable();
+                $table->timestamps();
+            });
+        }
+        if (!Schema::connection('shard')->hasTable('angaza_transactions')) {
+            Schema::connection('shard')->create('angaza_transactions', static function (Blueprint $table) {
+                $table->increments('id');
+                $table->timestamps();
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::connection('shard')->dropIfExists('angaza_api_credentials');
+        Schema::connection('shard')->dropIfExists('angaza_transactions');
+    }
+};
