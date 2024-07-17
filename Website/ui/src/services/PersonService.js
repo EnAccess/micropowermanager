9eed9f45f71e613d466b854875448a3398e143fc
Daniel Mohns <daniel.mohns@posteo.de>
2024-07-17 10:40:57 +0200

    Merge `Person` and `People` abstraction

--- Website/ui/src/services/PersonService.js
+++ Website/ui/src/services/PersonService.js
@@ -2,6 +2,129 @@ import Repository from '../repositories/RepositoryFactory'
 import { ErrorHandler } from '@/Helpers/ErrorHander'
 import moment from 'moment'
 import { convertObjectKeysToSnakeCase } from '@/Helpers/Utils'
+import { resources } from '@/resources'
+
+import { Paginator } from '@/classes/paginator'
+import { EventBus } from '@/shared/eventbus'
+export class Person {
+    constructor() {
+        this.id = null
+        this.title = null
+        this.education = null
+        this.birthDate = null
+        this.name = null
+        this.surname = null
+        this.gender = null
+        this.nationality = null
+        this.city = null
+        this.devices = []
+    }
+
+    initialize(personData) {
+        this.id = personData.id
+        this.title = personData.title
+        this.education = personData.education
+        this.birthDate = personData.birth_date
+        this.name = personData.name
+        this.surname = personData.surname
+        this.nationality =
+            personData.citizenship != null
+                ? personData.citizenship.country_name
+                : 'No data available'
+        this.gender = personData.sex
+        this.addresses = personData.addresses
+        this.devices = personData.devices
+
+        return this
+    }
+
+    updateName(fullName) {
+        let x = fullName.split(' ')
+        if (x.length < 2) {
+            return {
+                success: false,
+            }
+        }
+        this.surname = x.splice(-1)
+        this.name = x.join(' ')
+    }
+
+    fromJson(data) {
+        this.id = data.id
+        this.title = data.title
+        this.education = data.education
+        this.birthDate = data.birth_date
+        this.name = data.name
+        this.surname = data.surname
+        this.nationality =
+            data.citizenship != null
+                ? data.citizenship.country_name
+                : 'No data available'
+        this.gender = data.sex
+        this.addresses = data.addresses
+        this.lastUpdate = data.updated_at
+        this.devices = data.devices
+
+        return this
+    }
+
+    toJson() {
+        return {
+            title: this.title,
+            name: this.name,
+            surname: this.surname,
+            birth_date: this.birthDate,
+            sex: this.gender,
+            education: this.education,
+        }
+    }
+
+    isoYear(date) {
+        return moment(date).format('YYYY-MM-DD')
+    }
+
+    updatePerson() {
+        this.updateName(this.name)
+        if (this.birthDate !== null) {
+            this.birthDate = this.isoYear(this.birthDate)
+        }
+        axios.put(resources.person.update + this.id, this.toJson())
+    }
+
+    getFullName() {
+        return this.name + ' ' + this.surname
+    }
+
+    getId() {
+        return this.id
+    }
+}
+
+export class People {
+    constructor() {
+        this.list = []
+        this.paginator = new Paginator(resources.person.list)
+    }
+
+    search(term) {
+        this.paginator = new Paginator(resources.person.search)
+        EventBus.$emit('loadPage', this.paginator, { term: term })
+    }
+
+    showAll() {
+        this.paginator = new Paginator(resources.person.list)
+        EventBus.$emit('loadPage', this.paginator)
+    }
+
+    async updateList(data) {
+        this.list = []
+
+        for (let m in data) {
+            let person = new Person().fromJson(data[m])
+            this.list.push(person)
+        }
+    }
+}
 
 export class PersonService {
     constructor() {
