3648ede2f36f8e1ff98fd9da7ae51d6abc9075b0
Daniel Mohns <daniel.mohns@posteo.de>
2024-08-12 15:58:35 +0200

    Fix lint

--- Website/htdocs/mpmanager/app/Console/Commands/CreateDummyCompanyWithData.php
+++ Website/htdocs/mpmanager/app/Console/Commands/CreateDummyCompanyWithData.php
@@ -2,9 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Company;
-use App\Models\CompanyDatabase;
-use App\Models\DatabaseProxy;
 use App\Services\CompanyDatabaseService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
