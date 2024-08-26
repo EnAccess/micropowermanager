37aa4c4dd3d5c52d1c1a3c2dda7dbd5a09611aab
Daniel Mohns <daniel.mohns@posteo.de>
2024-08-26 15:48:09 +0200

    Add Tickets

--- Website/htdocs/mpmanager/packages/inensus/ticket/src/Models/TicketCategory.php
+++ Website/htdocs/mpmanager/packages/inensus/ticket/src/Models/TicketCategory.php
@@ -2,7 +2,18 @@
 
 namespace Inensus\Ticket\Models;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Inensus\Ticket\Factories\TicketCategoryFactory;
+
 class TicketCategory extends BaseModel
 {
+    /**
+     * Create a new factory instance for the model.
+     */
+    protected static function newFactory(): Factory
+    {
+        return TicketCategoryFactory::new();
+    }
+
     protected $table = 'ticket_categories';
 }
