16127847debbfe1cae3db7c27f44061d879fef29
Daniel Mohns <daniel.mohns@posteo.de>
2024-09-25 17:11:50 +0200

    Refactor Docker and Compose to be more consistent between dev and prod

--- Docker/entrypoint-dev.sh
+++ Docker/entrypoint-dev.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-cd /var/www/html/mpmanager
-#download composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
 
+cd /var/www/html/mpmanager
 composer install
 
 echo "Executing command: $@"
