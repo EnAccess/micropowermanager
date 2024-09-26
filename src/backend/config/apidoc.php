29aae5c523a3c4d0c03c42061484b612282560a1
Daniel Mohns <daniel.mohns@posteo.de>
2024-09-26 11:39:03 +0200

    Remove leftover local URL's

--- src/backend/config/apidoc.php
+++ src/backend/config/apidoc.php
@@ -53,7 +53,7 @@ return [
      * The base URL to be used in examples and the Postman collection.
      * By default, this will be the value of config('app.url').
      */
-    'base_url' => 'http://mpmanager.local',
+    'base_url' => 'http://localhost:8000',
 
     /*
      * Generate a Postman collection in addition to HTML docs.
