8ad7521447e0b4e15d880031afa1e9f5894a6e3d
Daniel Mohns <daniel.mohns@posteo.de>
2024-10-24 14:23:30 +0200

    Add `SolarHomeSystem` seeder

--- src/backend/app/Models/SolarHomeSystem.php
+++ src/backend/app/Models/SolarHomeSystem.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Models\Base\BaseModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class SolarHomeSystem extends BaseModel
 {
+    use HasFactory;
+
     public const RELATION_NAME = 'solar_home_system';
     protected $table = 'solar_home_systems';
 
