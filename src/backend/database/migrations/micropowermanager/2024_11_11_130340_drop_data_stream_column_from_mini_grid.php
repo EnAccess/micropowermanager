3a153796d1fc292171e1fb1e2a47f8424630b3e2
Daniel Mohns <daniel.mohns@posteo.de>
2024-11-11 14:07:59 +0100

    Add migration file

---
+++ src/backend/database/migrations/micropowermanager/2024_11_11_130340_drop_data_stream_column_from_mini_grid.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('shard')->table('mini_grids', function (Blueprint $table) {
+            $table->dropColumn('data_stream');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('shard')->table('mini_grids', function (Blueprint $table) {
+            $table->integer('data_stream')->default(0)->after('name');
+        });
+    }
+};
