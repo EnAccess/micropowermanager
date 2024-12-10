a4d67d910ace45da7272dfb1f68557251985b371
Delyce Twizeyimana <delyce35@gmail.com>
2024-12-10 16:50:20 +0200

    feat: switching to Demo to dedicated RDS maria db (#385)
    * feat: switching to RDS maria db
    
    * ch: changed database name configuration
    
    * updating secrets
    
    * shard config
    
    * updating secrets and formatting
    
    * delete unneccessary files
    
    * secrets
    
    * secrets
    
    * secrets
    
    * consistent naming
    
    * secrets

--- src/backend/app/modules/DatabaseProxy/DatabaseProxyManagerService.php
+++ src/backend/app/modules/DatabaseProxy/DatabaseProxyManagerService.php
@@ -40,16 +40,16 @@ class DatabaseProxyManagerService {
         $databaseConnections = config()->get('database.connections');
         $databaseConnections['shard'] = [
             'driver' => 'mysql',
-            'host' => 'db',
-            'port' => '3306',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
             'database' => $databaseName,
-            'username' => 'root',
-            'password' => env('DB_PASSWORD'),
-            'unix_socket' => '',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ];
         config()->set('database.connections', $databaseConnections);
