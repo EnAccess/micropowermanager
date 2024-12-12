16072a0d3e4a4329d5808f538f63143ccfd66b0f
Daniel Mohns <daniel.mohns@posteo.de>
2024-12-12 16:02:27 +0100

    Clean up `TicketSettings` seeders

--- src/backend/database/dummyData/dummy_ticket_maintenance_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ticket_settings` (`id`, `name`, `api_token`, `api_url`, `api_key`, `created_at`, `updated_at`)
-VALUES
-    (1,'Trello','----','https://api.trello.com/1','----','2023-10-10 11:43:51','2023-10-10 11:43:51');
