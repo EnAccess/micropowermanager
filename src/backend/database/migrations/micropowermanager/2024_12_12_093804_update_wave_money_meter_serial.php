f286ccbda836b1dec71985640a8e66f1db9e145a
Daniel Mohns <daniel.mohns@posteo.de>
2024-12-12 10:45:24 +0100

    Fix transaction data generation

---
+++ src/backend/database/migrations/micropowermanager/2024_12_12_093804_update_wave_money_meter_serial.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::connection('shard')->table('wave_money_transactions', static function (Blueprint $table) {
+            $table->string('meter_serial')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::connection('shard')->table('wave_money_transactions', function (Blueprint $table) {
+            $table->integer('meter_serial')->nullable()->change();
+        });
+    }
+};
