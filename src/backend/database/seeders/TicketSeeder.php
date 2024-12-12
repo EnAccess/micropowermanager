1a2506c2da67cd2d6d2d9d7f72d84c210494f425
Daniel Mohns <daniel.mohns@posteo.de>
2024-12-12 16:04:21 +0100

    Remove leftover

--- src/backend/database/seeders/TicketSeeder.php
+++ src/backend/database/seeders/TicketSeeder.php
@@ -129,8 +129,6 @@ class TicketSeeder extends Seeder {
                 ->for($minigrid)
                 ->for($agentPerson)
                 ->create();
-
-            // dd($maintenanceUserAgent);
         }
     }
 }
