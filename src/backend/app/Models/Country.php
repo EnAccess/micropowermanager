acf8da3b0ff0794505b4dce07a540c181b876f54
Obinna Ikeh <hikenike6@gmail.com>
2024-12-30 20:46:40 +0100

    Update /countries endpoint for use on the client #254

--- src/backend/app/Models/Country.php
+++ src/backend/app/Models/Country.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use App\Models\Base\BaseModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Country Class.
+ *
+ * @property int    $id
+ * @property string $country_code
+ * @property string $country_name
+ * **/
 class Country extends BaseModel {
+    use HasFactory;
+
     /**
      * @return string
      */
