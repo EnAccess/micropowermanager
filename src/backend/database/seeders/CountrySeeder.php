2d7fe55f85f714da16b97ba8b2dfdd665ad6f82d
Obinna Ikeh <hikenike6@gmail.com>
2024-12-30 20:45:33 +0100

    Create factory & seeder for Country model #254

---
+++ src/backend/database/seeders/CountrySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use MPM\DatabaseProxy\DatabaseProxyManagerService;
+
+class CountrySeeder extends Seeder {
+    public function __construct(
+        private DatabaseProxyManagerService $databaseProxyManagerService,
+    ) {
+        $this->databaseProxyManagerService->buildDatabaseConnectionDummyCompany();
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        $country = Storage::disk('local')->get('countries.json');
+        $countries = json_decode($country, true);
+
+        if (is_array($countries)) {
+            foreach ($countries as $code => $name) {
+                // Use the Country factory to create a new record
+                Country::factory()->create([
+                    'country_code' => $code,
+                    'country_name' => $name,
+                ]);
+            }
+        } else {
+            $this->command->error('Unable to decode countries.json. Please ensure it is valid JSON.');
+        }
+    }
+}
