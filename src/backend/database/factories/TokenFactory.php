dedcdaaef6b95b4b0c749f1f8b94c61eb0a47530
Obinna Ikeh <hikenike6@gmail.com>
2025-01-18 23:08:43 +0100

    refactor: replace query with factory calls(TransanctionSeeder)

---
+++ src/backend/database/factories/TokenFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Token;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TokenFactory extends Factory {
+    protected $model = Token::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+        return [
+            'transaction_id' => $this->faker->randomNumber(3),
+            'token' => $this->generateToken(),
+            'load' => 0.123,
+        ];
+    }
+
+    /**
+     * Generate a random 12-digit token.
+     *
+     * @return string
+     */
+    public static function generateToken() {
+        $token = '';
+        for ($i = 0; $i < 12; ++$i) {
+            $token .= random_int(0, 9);
+        }
+
+        return $token;
+    }
+}
