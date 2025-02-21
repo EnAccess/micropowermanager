a79f4127177615dc0d3bb48d617d7a0efb5e2726
Obinna Ikeh <hikenike6@gmail.com>
2025-02-21 14:54:46 +0100

    fix: update migration to use tenant connection for meter_parameters and social_tariff_piggy_banks

--- src/backend/database/migrations/micropowermanager/2025_01_04_120250_drop_meter_parameter_id_field_on_social_tariff_piggy_banks_table.php
+++ src/backend/database/migrations/micropowermanager/2025_01_04_120250_drop_meter_parameter_id_field_on_social_tariff_piggy_banks_table.php
@@ -11,11 +11,11 @@ return new class extends Migration {
      * @return void
      */
     public function up() {
-        Schema::connection('shard')->table('social_tariff_piggy_banks', function (Blueprint $table) {
-            if (Schema::connection('shard')->hasColumn('social_tariff_piggy_banks', 'meter_parameter_id')) {
+        Schema::connection('tenant')->table('social_tariff_piggy_banks', function (Blueprint $table) {
+            if (Schema::connection('tenant')->hasColumn('social_tariff_piggy_banks', 'meter_parameter_id')) {
                 $table->dropColumn('meter_parameter_id');
             }
-            if (!Schema::connection('shard')->hasColumn('social_tariff_piggy_banks', 'meter_id')) {
+            if (!Schema::connection('tenant')->hasColumn('social_tariff_piggy_banks', 'meter_id')) {
                 $table->integer('meter_id');
             }
         });
@@ -27,11 +27,11 @@ return new class extends Migration {
      * @return void
      */
     public function down() {
-        Schema::connection('shard')->table('social_tariff_piggy_banks', function (Blueprint $table) {
-            if (!Schema::connection('shard')->hasColumn('social_tariff_piggy_banks', 'meter_parameter_id')) {
+        Schema::connection('tenant')->table('social_tariff_piggy_banks', function (Blueprint $table) {
+            if (!Schema::connection('tenant')->hasColumn('social_tariff_piggy_banks', 'meter_parameter_id')) {
                 $table->integer('meter_parameter_id');
             }
-            if (Schema::connection('shard')->hasColumn('social_tariff_piggy_banks', 'meter_id')) {
+            if (Schema::connection('tenant')->hasColumn('social_tariff_piggy_banks', 'meter_id')) {
                 $table->dropColumn('meter_id');
             }
         });
