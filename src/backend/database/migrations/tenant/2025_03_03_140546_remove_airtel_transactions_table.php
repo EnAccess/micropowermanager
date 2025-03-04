13ac1ebbbe6a2f4e2c4c89bda0d0f1a329823758
“93Kamuran” <enginkamuran@gmail.com>
2025-03-04 10:38:54 +0300

    updated

--- src/backend/database/migrations/tenant/2025_03_03_140546_remove_airtel_transactions_table.php
+++ src/backend/database/migrations/tenant/2025_03_03_140546_remove_airtel_transactions_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -18,5 +19,20 @@ return new class extends Migration {
      *
      * @return void
      */
-    public function down() {}
+    public function down() {
+        Schema::connection('tenant')->create('airtel_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('interface_id');
+            $table->string('business_number');
+            $table->string('trans_id');
+            $table->string('tr_id');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::connection('tenant')->table('airtel_transactions', function (Blueprint $table) {
+            $table->string('manufacturer_transaction_type')->nullable();
+            $table->integer('manufacturer_transaction_id')->nullable();
+        });
+    }
 };
--- src/backend/database/migrations/tenant/2025_03_03_140646_remove_vodacom_transactions_table copy.php
+++ src/backend/database/migrations/tenant/2025_03_03_140646_remove_vodacom_transactions_table copy.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -18,5 +19,21 @@ return new class extends Migration {
      *
      * @return void
      */
-    public function down() {}
+    public function down() {
+        Schema::connection('tenant')->create('vodacom_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('conversation_id')->unique();
+            $table->string('originator_conversation_id')->unique();
+            $table->string('mpesa_receipt');
+            $table->dateTime('transaction_date');
+            $table->string('transaction_id')->unique();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::connection('tenant')->table('vodacom_transactions', function (Blueprint $table) {
+            $table->string('manufacturer_transaction_type')->nullable();
+            $table->integer('manufacturer_transaction_id')->nullable();
+        });
+    }
 };
