7798ff5e2b6defee3600732bd4f07b83490d0a44
Daniel Mohns <daniel.mohns@posteo.de>
2025-03-05 17:00:13 +0100

    Restore Email templates
    Accidentally removed here: https://github.com/EnAccess/micropowermanager/commit/641c009ef7cfbd3bf21c97ea2778d605b74e854e

---
+++ src/backend/resources/views/templates/mail/register_welcome.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Welcome to MicroPowerManager</title>
+</head>
+<body>
+Dear <strong>{{$userName}}</strong> from {{$companyName}},<br> thank you for registering. There are plenty of plugins that could help to optimize your business.
+Please checkout https://micropowermanager.com/. Here is also an introduction video of MPM: <a href="https://www.youtube.com/watch?v=egD0jV3b4dY">Youtube video</a>
+
+This is an automatic mail please don't reply to it.
+
+<br>
+<small>MicroPowerManager</small>
+
+</body>
+
+</html>
