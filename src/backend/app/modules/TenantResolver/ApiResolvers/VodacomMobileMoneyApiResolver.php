505e0b4f937a13cc254ce2aaf56cf4fe7c63198e
Obinna Ikeh <hikenike6@gmail.com>
2025-03-28 05:18:19 +0100

    Update Vodacom transaction request bodies and improve error handling in resources

--- src/backend/app/modules/TenantResolver/ApiResolvers/VodacomMobileMoneyApiResolver.php
+++ src/backend/app/modules/TenantResolver/ApiResolvers/VodacomMobileMoneyApiResolver.php
@@ -9,12 +9,11 @@ use Illuminate\Http\Request;
 
 class VodacomMobileMoneyApiResolver implements ApiResolverInterface {
     public function resolveCompanyId(Request $request): int {
-        $segments = $request->segments();
-        if (count($segments) < 6) {
+        $companyId = auth('api')->payload()->get('companyId');
+
+        if (!$companyId) {
             throw new ValidationException('failed to parse company identifier from the webhook');
         }
-
-        $companyId = $segments[5];
 
         return (int) $companyId;
     }
