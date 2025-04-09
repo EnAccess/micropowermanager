873316d0de6c9fcbe3c663eab5ffab603bd05e4b
“93Kamuran” <enginkamuran@gmail.com>
2025-04-09 12:31:18 +0300

    updated

--- src/backend/database/migrations/tenant/2025_04_08_227939_create_chint_meter_tables.php
+++ src/backend/database/migrations/tenant/2025_04_08_227939_create_chint_meter_tables.php
@@ -6,26 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up() {
-        if (!Schema::hasTable('chint_api_credentials')) {
-            Schema::create('chint_api_credentials', static function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('api_url')->default('https://197.221.155.210/jianpanbiaoEnglish/PrepaidWebService.asmx');
-                $table->string('user_name')->nullable();
-                $table->string('user_password')->nullable();
-                $table->timestamps();
-            });
-        }
+        Schema::connection('tenant')->create('chint_api_credentials', static function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('api_url')->default('https://197.221.155.210/jianpanbiaoEnglish/PrepaidWebService.asmx');
+            $table->string('user_name')->nullable();
+            $table->string('user_password')->nullable();
+            $table->timestamps();
+        });
 
-        if (!Schema::hasTable('chint_transactions')) {
-            Schema::create('chint_transactions', static function (Blueprint $table) {
-                $table->increments('id');
-                $table->timestamps();
-            });
-        }
+        Schema::connection('tenant')->create('chint_transactions', static function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
     }
 
     public function down() {
-        Schema::dropIfExists('chint_api_credentials');
-        Schema::dropIfExists('chint_transactions');
+        Schema::connection('tenant')->dropIfExists('chint_api_credentials');
+        Schema::connection('tenant')->dropIfExists('chint_transactions');
     }
 };
