e41316bc8b43928545fd11123cb28b80c6b8dbc5
alaincodes24 <alaincodes24@gmail.com>
2025-04-10 11:36:36 +0200

    Fix failing larastan issues

--- src/backend/app/modules/TenantResolver/ApiResolvers/SwiftaPaymentApiResolver.php
+++ src/backend/app/modules/TenantResolver/ApiResolvers/SwiftaPaymentApiResolver.php
@@ -3,9 +3,13 @@
 namespace MPM\TenantResolver\ApiResolvers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTGuard;
 
 class SwiftaPaymentApiResolver implements ApiResolverInterface {
     public function resolveCompanyId(Request $request): int {
-        return (int) auth()->payload()->get('companyId');
+        /** @var JWTGuard $guard */
+        $guard = auth('api');
+
+        return (int) $guard->payload()->get('companyId');
     }
 }
