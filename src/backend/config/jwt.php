f981a0ca02134a7188a59959f790b59572342552
alaincodes24 <alaincodes24@gmail.com>
2025-04-14 15:09:04 +0200

    Upgrade to laravel 10 (#578)
    * Update composer dependencies
    
    * Update dependencies and fix jwt related issues
    
    * Update composer.lock file
    
    * Add JWT secret key
    
    * Fix larastan CI/CD issues
    
    * Revert "Fix larastan CI/CD issues"
    
    This reverts commit a20ff6d9e28db456eedab040e02240eb67a65a05.
    
    * Update jwt secret
    
    * Fix secret is not set issue
    
    * Fix phpcs formatting issues
    
    * Fix companyId formatting issue
    
    * Fix phpcs issues
    
    * Fix failing larastan issues

--- src/backend/config/jwt.php
+++ src/backend/config/jwt.php
@@ -270,7 +270,7 @@ return [
         |
         */
 
-        'jwt' => Tymon\JWTAuth\Providers\JWT\Namshi::class,
+        'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
 
         /*
         |--------------------------------------------------------------------------
