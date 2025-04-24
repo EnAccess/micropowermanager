4ca1ae7c0c0ac1c19ed67e60119d01ad2f27cd38
Daniel Mohns <daniel.mohns@posteo.de>
2025-04-24 17:05:06 +0200

    Fix migration with string `null` values

--- src/backend/database/migrations/tenant/2025_04_24_140226_update_main_settings_protected_page_password.php
+++ src/backend/database/migrations/tenant/2025_04_24_140226_update_main_settings_protected_page_password.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,11 +14,15 @@ return new class extends Migration {
      */
     public function up() {
         DB::connection('tenant')->table('main_settings')
+            // It's not fully clear how it happened, but after
+            // https://github.com/EnAccess/micropowermanager/pull/659
+            // `protected_page_password` was sometimes set to string `'null'`
+            // and sometimes to SQL `NULL`
             ->where(function ($query) {
                 $query->whereNull('protected_page_password')
                     ->orWhere('protected_page_password', 'null');
             })
-            ->update(['protected_page_password' => '123123']);
+            ->update(['protected_page_password' => Crypt::encrypt('123123')]);
 
         Schema::connection('tenant')->table('main_settings', function (Blueprint $table) {
             $table->longText('protected_page_password')->nullable(false)->change();
