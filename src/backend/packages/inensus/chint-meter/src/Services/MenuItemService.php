3485ebd38c6b6c9b781dc16f919a4c05a4475a06
“93Kamuran” <enginkamuran@gmail.com>
2025-05-02 14:56:31 +0300

    sidebar issue fix

---
+++ src/backend/packages/inensus/chint-meter/src/Services/MenuItemService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Inensus\ChintMeter\Services;
+
+
+class MenuItemService
+{
+    const MENU_ITEM = 'Chint Meter';
+
+    public function createMenuItems()
+    {
+        $menuItem = [
+            'name' => 'Chint Meter',
+            'url_slug' => '',
+            'md_icon' => 'bolt'
+        ];
+        $subMenuItems = array();
+
+        $subMenuItem1 = [
+            'name' => 'Overview',
+            'url_slug' => '/chint-meters/chint-overview',
+        ];
+        array_push($subMenuItems, $subMenuItem1);
+
+        return ['menuItem' => $menuItem, 'subMenuItems' => $subMenuItems];
+
+    }
+}
