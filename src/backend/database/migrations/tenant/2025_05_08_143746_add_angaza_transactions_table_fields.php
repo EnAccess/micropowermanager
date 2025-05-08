94fdac261b79c78883b9a4996d3186a2a7258a9e
Obinna Ikeh <hikenike6@gmail.com>
2025-05-08 21:55:40 +0100

    Fix linting warnings

--- src/backend/database/migrations/tenant/2025_05_08_143746_add_angaza_transactions_table_fields.php
+++ src/backend/database/migrations/tenant/2025_05_08_143746_add_angaza_transactions_table_fields.php
@@ -4,18 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAngazaTransactionsTableFields extends Migration
-{
-    public function up()
-    {
+class AddAngazaTransactionsTableFields extends Migration {
+    public function up() {
         Schema::table('angaza_transactions', function (Blueprint $table) {
             $table->integer('status')->default(-1);
             $table->decimal('amount', 10, 2);
         });
     }
 
-    public function down()
-    {
+    public function down() {
         Schema::table('angaza_transactions', function (Blueprint $table) {
             $table->dropColumn(['status', 'amount']);
         });
--- src/backend/database/migrations/tenant/2025_05_08_144052_add_sun_king_transactions_table_fields.php
+++ src/backend/database/migrations/tenant/2025_05_08_144052_add_sun_king_transactions_table_fields.php
@@ -4,20 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSunKingTransactionsTableFields extends Migration
-{
-    public function up()
-    {
+class AddSunKingTransactionsTableFields extends Migration {
+    public function up() {
         Schema::table('sun_king_transactions', function (Blueprint $table) {
             $table->integer('status')->default(-1)->after('id');
             $table->decimal('amount', 10, 2)->after('status');
         });
     }
 
-    public function down()
-    {
+    public function down() {
         Schema::table('sun_king_transactions', function (Blueprint $table) {
             $table->dropColumn(['status', 'amount']);
         });
     }
-} 
+}
