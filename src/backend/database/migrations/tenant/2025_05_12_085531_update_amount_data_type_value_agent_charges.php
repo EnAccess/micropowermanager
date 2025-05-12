31df28f898ab735aff403f7cea9340680744c402
alaincodes24 <alaincodes24@gmail.com>
2025-05-12 11:42:56 +0200

    Fix code formatting issues

--- src/backend/database/migrations/tenant/2025_05_12_085531_update_amount_data_type_value_agent_charges.php
+++ src/backend/database/migrations/tenant/2025_05_12_085531_update_amount_data_type_value_agent_charges.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return  new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::connection('tenant')->table('agent_charges', function (Blueprint $table) {
             $table->double('amount')->change();
         });
@@ -23,8 +21,7 @@ return  new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::connection('tenant')->table('agent_charges', function (Blueprint $table) {
             $table->float('amount')->change();
         });
