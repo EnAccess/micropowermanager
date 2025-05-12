be9c9f0aeeef61acfa0749f499451a5712a125e5
Daniel Mohns <dmohns@users.noreply.github.com>
2025-05-12 10:53:00 +0200

    Fix person meter endpoints (#706)
    * Fix Device relation in `PersonMeter` endpoints
    
    * Add `PersonMeter` collections

--- src/backend/app/Services/PersonMeterService.php
+++ src/backend/app/Services/PersonMeterService.php
@@ -10,10 +10,10 @@ class PersonMeterService {
     ) {}
 
     public function getPersonMeters(int $personId) {
-        return $this->person->newQuery()->with(['devices.device.tariff', 'meters.meter'])->find($personId);
+        return $this->person->newQuery()->with(['devices.device', 'devices.device.tariff'])->find($personId);
     }
 
     public function getPersonMetersGeographicalInformation(int $personId) {
-        return $this->person->newQuery()->with(['devices.device.meter', 'meters.geo'])->find($personId);
+        return $this->person->newQuery()->with(['devices.device', 'devices.address.geo'])->find($personId);
     }
 }
