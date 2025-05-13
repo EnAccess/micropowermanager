614958cb5d09c66eee9c6392ac53c2fbfedc39a3
alaincodes24 <alaincodes24@gmail.com>
2025-05-13 11:51:42 +0200

    Replace MeterParameter with Device relation in SparkMeter plugin (#705)
    * Replace MeterParameter with Device relation in SparkMeter plugin
    
    * Fix phpcs issues
    
    * Replace deprecated meters relation with devices.device
    
    * chore: trigger workflow

--- src/backend/packages/inensus/spark-meter/src/Observers/GeographicalInformationObserver.php
+++ src/backend/packages/inensus/spark-meter/src/Observers/GeographicalInformationObserver.php
@@ -2,6 +2,7 @@
 
 namespace Inensus\SparkMeter\Observers;
 
+use App\Models\Device;
 use App\Models\GeographicalInformation;
 use App\Models\MpmPlugin;
 use App\Models\Person\Person;
@@ -20,36 +21,72 @@ class GeographicalInformationObserver {
     ) {}
 
     public function updated(GeographicalInformation $geographicalInformation) {
-        if ($this->pluginsService->isPluginActive(MpmPlugin::SPARK_METER) && $geographicalInformation->owner_type === 'meter_parameter') {
-            $meterId = $geographicalInformation->owner_id;
-            $customer = $this->person->newQuery()->with(['meters.tariff', 'meters.geo', 'meters.meter'])
-                ->whereHas('meters', function ($q) use ($meterId) {
-                    return $q->where('id', $meterId);
-                })->first();
-            $smCustomer = $this->smCustomer->newQuery()
-                ->where('mpm_customer_id', $customer->id)->first();
-            if ($smCustomer) {
-                $address = $customer->addresses()->where('is_primary', 1)->first();
-                $customerData = [
-                    'id' => $smCustomer->customer_id,
-                    'active' => true,
-                    'meter_tariff_name' => $customer->meters[0]->tariff->name,
-                    'name' => $customer->name.' '.$customer->surname,
-                    'code' => strval($customer->id),
-                    'phone_number' => $address->phone,
-                    'coords' => $customer->meters[0]->geo->points,
-                    'address' => $address->street,
-                ];
+        if (!$this->pluginsService->isPluginActive(MpmPlugin::SPARK_METER)) {
+            return;
+        }
 
-                $this->customerService->updateSparkCustomerInfo($customerData, $smCustomer->site_id);
-                $smModelHash = $this->customerService->hashCustomerWithMeterSerial(
-                    $customer->meters[0]->meter->serial_number,
-                    $smCustomer->site_id
-                );
-                $smCustomer->update([
-                    'hash' => $smModelHash,
-                ]);
+        if ($geographicalInformation->owner_type === 'address') {
+            $address = $geographicalInformation->owner;
+
+            if ($address && $address->owner_type === 'device') {
+                $device = $address->owner;
+
+                if ($device && $device->device_type === 'meter') {
+                    $this->updateSparkMetaCustomerInformation($device, $geographicalInformation);
+                }
             }
         }
+    }
+
+    /**
+     * Update Spark meter customer information.
+     *
+     * @param Device                  $device
+     * @param GeographicalInformation $geographicalInformation
+     *
+     * @return void
+     */
+    private function updateSparkMetaCustomerInformation(Device $device, GeographicalInformation $geographicalInformation) {
+        $meter = $device->device;
+
+        $customer = $this->person->newQuery()
+            ->with(['devices.device.tariff', 'devices.address.geo'])
+            ->whereHas('devices', function ($q) use ($device) {
+                return $q->where('id', $device->id);
+            })->first();
+
+        if (!$customer) {
+            return;
+        }
+
+        $smCustomer = $this->smCustomer->newQuery()
+            ->where('mpm_customer_id', $customer->id)
+            ->first();
+
+        if (!$smCustomer) {
+            return;
+        }
+
+        $primaryAddress = $customer->addresses()->where('is_primary', 1)->first();
+        $customerData = [
+            'id' => $smCustomer->customer_id,
+            'active' => true,
+            'meter_tariff_name' => $customer->devices[0]->device->tariff->name,
+            'name' => $customer->name.' '.$customer->surname,
+            'code' => strval($customer->id),
+            'phone_number' => $primaryAddress->phone,
+            'coords' => $geographicalInformation->points,
+            'address' => $primaryAddress->street,
+        ];
+
+        $this->customerService->updateSparkCustomerInfo($customerData, $smCustomer->site_id);
+        $smModelHash = $this->customerService->hashCustomerWithMeterSerial(
+            $meter->serial_number,
+            $smCustomer->site_id
+        );
+
+        $smCustomer->update([
+            'hash' => $smModelHash,
+        ]);
     }
 }
