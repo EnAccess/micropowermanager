225499614209740dff6d34a50c902cd8dc583813
alaincodes24 <alaincodes24@gmail.com>
2025-05-21 09:30:34 +0200

    Fix phpcs code formatting issues

--- src/backend/database/migrations/tenant/2025_05_21_071344_rename_device_id_to_mobile_device_id_in_agent_transactions_table.php
+++ src/backend/database/migrations/tenant/2025_05_21_071344_rename_device_id_to_mobile_device_id_in_agent_transactions_table.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class  extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::connection('tenant')->table('agent_transactions', function (Blueprint $table) {
             $table->renameColumn('device_id', 'mobile_device_id');
         });
@@ -23,8 +21,7 @@ return new class  extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::connection('tenant')->table('agent_transactions', function (Blueprint $table) {
             $table->renameColumn('mobile_device_id', 'device_id');
         });
