18a1b2c04a49b9f60791361700d9f774c778b1e8
alaincodes24 <alaincodes24@gmail.com>
2025-05-22 21:34:40 +0200

    Refactor code to comply with laravel 11

--- src/backend/bootstrap/app.php
+++ src/backend/bootstrap/app.php
@@ -1,55 +1,44 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+use App\Http\Middleware\AdminJWT;
+use App\Http\Middleware\AgentBalanceMiddleware;
+use App\Http\Middleware\JwtMiddleware;
+use App\Http\Middleware\Transaction;
+use App\Http\Middleware\TransactionRequest;
+use App\Http\Middleware\UserDefaultDatabaseConnectionMiddleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Throwable;
 
-$app = new Illuminate\Foundation\Application(
-    realpath(__DIR__.'/../')
-);
-
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
-
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
+return Application::configure(basePath: dirname(__DIR__))
+    ->withSingletons([
+        Illuminate\Contracts\Console\Kernel::class => App\Console\Kernel::class,
+        Illuminate\Contracts\Debug\ExceptionHandler::class =>
     App\Exceptions\Handler::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
-
-return $app;
+    ])
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use([
+            UserDefaultDatabaseConnectionMiddleware::class,
+        ]);
+        $middleware->appendToGroup('agent_api', [
+            'bindings',
+        ]);
+        $middleware->alias([
+            'bindings' => SubstituteBindings::class,
+            'transaction.auth' => Transaction::class,
+            'transaction.request' => TransactionRequest::class,
+            'admin' => AdminJWT::class,
+            'jwt.verify' => JwtMiddleware::class,
+            'agent.balance' => AgentBalanceMiddleware::class,
+        ]);
+    })
+    ->create();
--- src/backend/config/app.php
+++ src/backend/config/app.php
@@ -109,83 +109,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    'providers' => [
-        /*
-         * Laravel Framework Service Providers...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        App\Providers\ServicesProvider::class, // for helper services
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-        Inensus\Ticket\Providers\TicketServiceProvider::class,
-        Inensus\SparkMeter\Providers\SparkMeterServiceProvider::class,
-        Inensus\SteamaMeter\Providers\SteamaMeterServiceProvider::class,
-        Inensus\KelinMeter\Providers\KelinMeterServiceProvider::class,
-        Inensus\CalinMeter\Providers\CalinMeterServiceProvider::class,
-        Inensus\StronMeter\Providers\StronMeterServiceProvider::class,
-        Inensus\CalinSmartMeter\Providers\CalinSmartMeterServiceProvider::class,
-        Inensus\SwiftaPaymentProvider\Providers\SwiftaServiceProvider::class,
-        Inensus\MesombPaymentProvider\Providers\MesombServiceProvider::class,
-        Inensus\BulkRegistration\Providers\BulkRegistrationServiceProvider::class,
-        Inensus\ViberMessaging\Providers\ViberMessagingServiceProvider::class,
-        Inensus\WaveMoneyPaymentProvider\Providers\WaveMoneyPaymentProviderServiceProvider::class,
-        Inensus\MicroStarMeter\Providers\MicroStarMeterServiceProvider::class,
-        Inensus\SunKingSHS\Providers\SunKingSHSServiceProvider::class,
-        Inensus\GomeLongMeter\Providers\GomeLongMeterServiceProvider::class,
-        Inensus\WavecomPaymentProvider\Providers\WavecomPaymentProviderServiceProvider::class,
-        Inensus\AngazaSHS\Providers\AngazaSHSServiceProvider::class,
-        Inensus\DalyBms\Providers\DalyBmsServiceProvider::class,
-        Inensus\AfricasTalking\Providers\AfricasTalkingServiceProvider::class,
-        Inensus\VodacomMobileMoney\Providers\VodacomMobileMoneyServiceProvider::class,
-        Inensus\ChintMeter\Providers\ChintMeterServiceProvider::class,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Class Aliases
     |--------------------------------------------------------------------------
     |
@@ -196,39 +119,6 @@ return [
     */
 
     'aliases' => [
-        'App' => Illuminate\Support\Facades\App::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
