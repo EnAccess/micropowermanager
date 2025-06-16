8b6afd6ab4578ebc26cfdf7b172ea8f04eb29860
Daniel Mohns <daniel.mohns@posteo.de>
2025-06-16 13:49:26 +0200

    Fix MySQL datatype update `float` -> `double`

--- src/backend/database/migrations/tenant/2025_05_12_091643_update_data_type_values_main_settings.php
+++ src/backend/database/migrations/tenant/2025_05_12_091643_update_data_type_values_main_settings.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -15,6 +16,12 @@ return new class extends Migration {
             $table->double('vat_energy')->change();
             $table->double('vat_appliance')->change();
         });
+
+        // For MySQL Laravel's `float(5)` gets translated to DOUBLE(5,2).
+        // As a result changing it to `double` using Laravel might not have an effect.
+        // Running a SQL statement here to be sure.
+        DB::connection('tenant')->statement('ALTER TABLE main_settings MODIFY vat_energy DOUBLE');
+        DB::connection('tenant')->statement('ALTER TABLE main_settings MODIFY vat_appliance DOUBLE');
     }
 
     /**
@@ -24,8 +31,11 @@ return new class extends Migration {
      */
     public function down() {
         Schema::connection('tenant')->table('main_settings', function (Blueprint $table) {
-            $table->float('vat_energy')->change();
-            $table->float('vat_appliance')->change();
+            $table->float('vat_energy', 5)->change();
+            $table->float('vat_appliance', 5)->change();
         });
+
+        DB::connection('tenant')->statement('ALTER TABLE main_settings MODIFY vat_energy DOUBLE(5,2)');
+        DB::connection('tenant')->statement('ALTER TABLE main_settings MODIFY vat_appliance DOUBLE(5,2)');
     }
 };
