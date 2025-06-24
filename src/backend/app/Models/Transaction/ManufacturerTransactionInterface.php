5b6de68ec887c11ac57af8bcde786a8b40088d2a
Daniel Mohns <daniel.mohns@posteo.de>
2025-06-24 15:05:31 +0200

    Add PHPDoc

--- src/backend/app/Models/Transaction/ManufacturerTransactionInterface.php
+++ src/backend/app/Models/Transaction/ManufacturerTransactionInterface.php
@@ -2,6 +2,19 @@
 
 namespace App\Models\Transaction;
 
+/**
+ * Interface ManufacturerTransactionInterface.
+ *
+ * Represents an abstraction over manufacturer-specific transaction implementations.
+ *
+ * Each method provides access to a specific payment provider transaction record that corresponds
+ * to how the payment is processed by the corresponding payment provider.
+ *
+ *  Implementations may also expose additional metadata such as `site_ids`,
+ * `external_ids`, device identifiers, or configuration parameters required
+ * by the manufacturer’s systems.
+ * These are should be exposed through custom methods or attributes on the implementing model.
+ */
 interface ManufacturerTransactionInterface {
     public function agentTransaction();
 
