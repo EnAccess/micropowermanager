1771f7b376276c3c28c259307f5e18e43006a165
alaincodes24 <alaincodes24@gmail.com>
2025-06-25 10:16:08 +0200

    Remove unneeded health route in favor of laravel built-in route

--- src/backend/bootstrap/app.php
+++ src/backend/bootstrap/app.php
@@ -23,7 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
-        // health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(UserDefaultDatabaseConnectionMiddleware::class);
