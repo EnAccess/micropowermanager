a389414a24a764a9a13e6bedf7611adba8b1945c
Obinna Ikeh <hikenike6@gmail.com>
2025-06-29 19:52:38 +0100

    Remove unused services on MainSettingsController

--- src/backend/app/Http/Controllers/MainSettingsController.php
+++ src/backend/app/Http/Controllers/MainSettingsController.php
@@ -4,16 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ApiResource;
 use App\Models\MainSettings;
-use App\Services\CompanyService;
 use App\Services\MainSettingsService;
-use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class MainSettingsController extends Controller {
     public function __construct(
         private MainSettingsService $mainSettingsService,
-        private UserService $userService,
-        private CompanyService $companyService,
     ) {}
 
     public function index(): ApiResource {
