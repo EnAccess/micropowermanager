317975c3a46a0e0275d26b00ef7c50b749b09822
Obinna Ikeh <hikenike6@gmail.com>
2025-06-29 13:24:42 +0100

    migrate passwords to main_settings

--- src/backend/database/migrations/tenant/2025_04_16_231252_add_protected_page_password_to_main_settings.php
+++ src/backend/database/migrations/tenant/2025_04_16_231252_add_protected_page_password_to_main_settings.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -11,8 +13,18 @@ return new class extends Migration {
      * @return void
      */
     public function up() {
-        Schema::connection('tenant')->table('main_settings', function (Blueprint $table) {
-            $table->longText('protected_page_password')->nullable();
+        $existingPasswords = DB::connection('micro_power_manager')->table('companies')
+            ->whereNotNull('protected_page_password')
+            ->pluck('protected_page_password', 'name');
+
+        foreach ($existingPasswords as $companyName => $password) {
+            DB::connection('tenant')
+                ->table('main_settings')
+                ->where('company_name', $companyName)
+                ->update(['protected_page_password' => $password]);
+        }
+        Schema::connection('micro_power_manager')->table('companies', function (Blueprint $table) {
+            $table->dropColumn('protected_page_password');
         });
     }
 
@@ -22,6 +34,19 @@ return new class extends Migration {
      * @return void
      */
     public function down() {
+        Schema::connection('micro_power_manager')->table('companies', function (Blueprint $table) {
+            $table->string('protected_page_password')->nullable();
+        });
+        $mainSettingsPasswords = DB::connection('tenant')
+            ->table('main_settings')
+            ->whereNotNull('protected_page_password')
+            ->pluck('protected_page_password', 'name');
+
+        foreach ($mainSettingsPasswords as $companyName => $password) {
+            DB::connection('micro_power_manager')->table('companies')
+                ->where('name', $companyName)
+                ->update(['protected_page_password' => $password]);
+        }
         Schema::connection('tenant')->table('main_settings', function (Blueprint $table) {
             $table->dropColumn('protected_page_password');
         });
