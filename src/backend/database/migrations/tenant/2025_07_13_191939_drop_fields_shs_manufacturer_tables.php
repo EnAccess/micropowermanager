ff599635407ed600c659677512673b070dcceb9f
Obinna Ikeh <hikenike6@gmail.com>
2025-07-13 22:06:50 +0100

    Fix  lint warning

--- src/backend/database/migrations/tenant/2025_07_13_191939_drop_fields_shs_manufacturer_tables.php
+++ src/backend/database/migrations/tenant/2025_07_13_191939_drop_fields_shs_manufacturer_tables.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return  new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('angaza_transactions', function (Blueprint $table) {
             $table->dropColumn(['status', 'amount']);
         });
@@ -27,8 +25,7 @@ return  new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('angaza_transactions', function (Blueprint $table) {
             $table->integer('status')->default(-1)->after('id');
             $table->decimal('amount', 10, 2)->after('status');
