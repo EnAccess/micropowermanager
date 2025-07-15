a98992fd66693c271510ed985dcdd77d4f3b2cc3
Daniel Mohns <daniel.mohns@posteo.de>
2025-07-15 17:28:15 +0200

    Move providers to `bootstrap/providers`

---
+++ src/backend/bootstrap/providers.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    /*
+    * Laravel Framework Service Providers...
+    */
+    Illuminate\Auth\AuthServiceProvider::class,
+    Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    Illuminate\Bus\BusServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
+    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+    Illuminate\Cookie\CookieServiceProvider::class,
+    Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\Encryption\EncryptionServiceProvider::class,
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+    Illuminate\Hashing\HashServiceProvider::class,
+    Illuminate\Mail\MailServiceProvider::class,
+    Illuminate\Notifications\NotificationServiceProvider::class,
+    Illuminate\Pagination\PaginationServiceProvider::class,
+    Illuminate\Pipeline\PipelineServiceProvider::class,
+    Illuminate\Queue\QueueServiceProvider::class,
+    Illuminate\Redis\RedisServiceProvider::class,
+    Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+    Illuminate\Session\SessionServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
+    Illuminate\Validation\ValidationServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
+
+    /*
+    * Package Service Providers...
+    */
+
+    /*
+    * Application Service Providers...
+    */
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\BroadcastServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+
+    App\Providers\ServicesProvider::class, // for helper services
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+    Inensus\Ticket\Providers\TicketServiceProvider::class,
+    Inensus\SparkMeter\Providers\SparkMeterServiceProvider::class,
+    Inensus\SteamaMeter\Providers\SteamaMeterServiceProvider::class,
+    Inensus\KelinMeter\Providers\KelinMeterServiceProvider::class,
+    Inensus\CalinMeter\Providers\CalinMeterServiceProvider::class,
+    Inensus\StronMeter\Providers\StronMeterServiceProvider::class,
+    Inensus\CalinSmartMeter\Providers\CalinSmartMeterServiceProvider::class,
+    Inensus\SwiftaPaymentProvider\Providers\SwiftaServiceProvider::class,
+    Inensus\MesombPaymentProvider\Providers\MesombServiceProvider::class,
+    Inensus\BulkRegistration\Providers\BulkRegistrationServiceProvider::class,
+    Inensus\ViberMessaging\Providers\ViberMessagingServiceProvider::class,
+    Inensus\WaveMoneyPaymentProvider\Providers\WaveMoneyPaymentProviderServiceProvider::class,
+    Inensus\MicroStarMeter\Providers\MicroStarMeterServiceProvider::class,
+    Inensus\SunKingSHS\Providers\SunKingSHSServiceProvider::class,
+    Inensus\GomeLongMeter\Providers\GomeLongMeterServiceProvider::class,
+    Inensus\WavecomPaymentProvider\Providers\WavecomPaymentProviderServiceProvider::class,
+    Inensus\AngazaSHS\Providers\AngazaSHSServiceProvider::class,
+    Inensus\DalyBms\Providers\DalyBmsServiceProvider::class,
+    Inensus\AfricasTalking\Providers\AfricasTalkingServiceProvider::class,
+    Inensus\VodacomMobileMoney\Providers\VodacomMobileMoneyServiceProvider::class,
+    Inensus\ChintMeter\Providers\ChintMeterServiceProvider::class,
+];
