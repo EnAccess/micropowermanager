ad646432ceae917296c5aa3ba49e3afd64ed235d
Daniel Mohns <dmohns@users.noreply.github.com>
2025-07-16 17:23:21 +0200

    Re-factor provider loading based in Laravel11 recommendation (#807)
    * Move providers to `bootstrap/providers`
    
    * Remove auto-loaded Laravel providers

---
+++ src/backend/bootstrap/providers.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    /*
+    * Application Service Providers...
+    */
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\BroadcastServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+
+    App\Providers\ServicesProvider::class, // for helper services
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+    Inensus\Ticket\Providers\TicketServiceProvider::class,
+    Inensus\SparkMeter\Providers\SparkMeterServiceProvider::class,
+    Inensus\SteamaMeter\Providers\SteamaMeterServiceProvider::class,
+    Inensus\KelinMeter\Providers\KelinMeterServiceProvider::class,
+    Inensus\CalinMeter\Providers\CalinMeterServiceProvider::class,
+    Inensus\StronMeter\Providers\StronMeterServiceProvider::class,
+    Inensus\CalinSmartMeter\Providers\CalinSmartMeterServiceProvider::class,
+    Inensus\SwiftaPaymentProvider\Providers\SwiftaServiceProvider::class,
+    Inensus\MesombPaymentProvider\Providers\MesombServiceProvider::class,
+    Inensus\BulkRegistration\Providers\BulkRegistrationServiceProvider::class,
+    Inensus\ViberMessaging\Providers\ViberMessagingServiceProvider::class,
+    Inensus\WaveMoneyPaymentProvider\Providers\WaveMoneyPaymentProviderServiceProvider::class,
+    Inensus\MicroStarMeter\Providers\MicroStarMeterServiceProvider::class,
+    Inensus\SunKingSHS\Providers\SunKingSHSServiceProvider::class,
+    Inensus\GomeLongMeter\Providers\GomeLongMeterServiceProvider::class,
+    Inensus\WavecomPaymentProvider\Providers\WavecomPaymentProviderServiceProvider::class,
+    Inensus\AngazaSHS\Providers\AngazaSHSServiceProvider::class,
+    Inensus\DalyBms\Providers\DalyBmsServiceProvider::class,
+    Inensus\AfricasTalking\Providers\AfricasTalkingServiceProvider::class,
+    Inensus\VodacomMobileMoney\Providers\VodacomMobileMoneyServiceProvider::class,
+    Inensus\ChintMeter\Providers\ChintMeterServiceProvider::class,
+];
