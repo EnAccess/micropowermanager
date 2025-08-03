602face481ce097096db72b422238e524777f3fc
alaincodes24 <alaincodes24@gmail.com>
2025-08-03 20:20:03 +0200

    Fix more phpstan issues on level 6

--- src/backend/app/Models/Address/HasAddressesInterface.php
+++ src/backend/app/Models/Address/HasAddressesInterface.php
@@ -3,7 +3,12 @@
 namespace App\Models\Address;
 
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 
 interface HasAddressesInterface {
+    /**
+     * @return HasOneOrMany<Address, Model, Collection<int, Address>>
+     */
     public function addresses(): HasOneOrMany;
 }
--- src/backend/app/Models/AgentAssignedAppliances.php
+++ src/backend/app/Models/AgentAssignedAppliances.php
@@ -7,23 +7,40 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @extends HasFactory<\Database\Factories\AgentAssignedAppliancesFactory>
+ */
 class AgentAssignedAppliances extends BaseModel {
+    /** @use HasFactory<\Database\Factories\AgentAssignedAppliancesFactory> */
     use HasFactory;
+
     public const RELATION_NAME = 'agent_appliance';
     protected $guarded = [];
 
+    /**
+     * @return BelongsTo<Agent, $this>
+     */
     public function agent(): BelongsTo {
         return $this->belongsTo(Agent::class);
     }
 
+    /**
+     * @return BelongsTo<User, $this>
+     */
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return BelongsTo<Asset, $this>
+     */
     public function appliance(): BelongsTo {
         return $this->belongsTo(Asset::class, 'appliance_id', 'id');
     }
 
+    /**
+     * @return HasMany<AgentSoldAppliance, $this>
+     */
     public function soldAppliance(): HasMany {
         return $this->hasMany(AgentSoldAppliance::class);
     }
