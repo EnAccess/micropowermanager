9954bb901f311d8bdda1a6ea2cb3914a6342db62
Daniel Mohns <daniel.mohns@posteo.de>
2025-08-04 13:15:51 +0200

    Make `ar` selectable

--- src/frontend/src/i18n.js
+++ src/frontend/src/i18n.js
@@ -1,6 +1,7 @@
+import ar from "../src/assets/locales/ar.json"
+import bu from "../src/assets/locales/bu.json"
 import en from "../src/assets/locales/en.json"
 import fr from "../src/assets/locales/fr.json"
-import bu from "../src/assets/locales/bu.json"
 import VueI18n from "vue-i18n"
 import Vue from "vue"
 
@@ -9,9 +10,10 @@ Vue.use(VueI18n)
 const i18n = new VueI18n({
   locale: localStorage.getItem("lang") || "en",
   messages: {
+    ar: ar,
+    bu: bu,
     en: en,
     fr: fr,
-    bu: bu,
   },
 })
 
