b01b44149d87ba533959d1cf0495298417db8f08
Daniel Mohns <daniel.mohns@posteo.de>
2025-08-21 18:09:48 +0200

    Simplify permission handling

--- docker/entrypoint-queue-worker.sh
+++ docker/entrypoint-queue-worker.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+echo "Ensure correct filesystem permissions are set..."
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html/storage
+
+# DEVCONTAINER PLACEHOLDER (DO NOT REMOVE THIS LINE)
+
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
