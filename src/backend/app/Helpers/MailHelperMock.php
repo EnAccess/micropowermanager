394f5aba31009e8514d22b3f19375c951a3d6fb8
Obinna Ikeh <hikenike6@gmail.com>
2025-08-26 00:39:16 +0100

    Refactor MailHelper to use Symfony Mailer, removing PHPMailer dependency

--- src/backend/app/Helpers/MailHelperMock.php
+++ src/backend/app/Helpers/MailHelperMock.php
@@ -2,12 +2,16 @@
 
 namespace App\Helpers;
 
+use App\Exceptions\MailNotSentException;
+
 class MailHelperMock implements MailHelperInterface {
     /**
      * @param string      $to
      * @param string      $title
      * @param string      $body
      * @param string|null $attachment
+     *
+     * @throws MailNotSentException
      */
     public function sendPlain(string $to, string $title, string $body, ?string $attachment = null): void {
         return;
