4caf827d21f6cccb47cc649e1cdd93bab6fe1be8
Daniel Mohns <daniel.mohns@posteo.de>
2025-09-08 11:49:05 +0200

    Add `rector/rector`

---
+++ src/backend/rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/packages',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(1000);
+// ->withSetProviders(LaravelSetProvider::class)
+// ->withComposerBased(laravel: true/* other options */)
+// ->withSets([
+//     LaravelSetList::LARAVEL_CODE_QUALITY,
+//     LaravelSetList::LARAVEL_COLLECTION,
+// ]);
