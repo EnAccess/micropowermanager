ef892db155336402f5d088c351483f757dd436f2
Obinna Ikeh <hikenike6@gmail.com>
2025-09-13 01:19:55 +0100

    Fix lint

--- src/backend/app/Http/Middleware/UserDefaultDatabaseConnectionMiddleware.php
+++ src/backend/app/Http/Middleware/UserDefaultDatabaseConnectionMiddleware.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
