d9bc6ec626620c869513d118b18b3490dfe79140
alaincodes24 <alaincodes24@gmail.com>
2025-09-29 22:30:25 +0200

    Add ProspectApiResolver

---
+++ src/backend/app/modules/TenantResolver/ApiResolvers/ProspectApiResolver.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MPM\TenantResolver\ApiResolvers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class ProspectApiResolver implements ApiResolverInterface {
+    public function resolveCompanyId(Request $request): int {
+        /** @var \Tymon\JWTAuth\JWTGuard $guard */
+        $guard = auth('api');
+        $payload = $guard->check() ? $guard->payload() : null;
+
+        $companyId = $payload?->get('companyId');
+
+        if (!$companyId) {
+            throw ValidationException::withMessages(['webhook' => 'failed to parse company identifier from the webhook']);
+        }
+
+        return (int) $companyId;
+    }
+}
