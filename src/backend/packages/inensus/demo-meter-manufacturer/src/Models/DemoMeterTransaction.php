220c5b2070a09364db4a97d4ec2265fdab6a4cfa
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-13 11:41:09 +0200

    Add types for Demo Plugins

--- src/backend/packages/inensus/demo-meter-manufacturer/src/Models/DemoMeterTransaction.php
+++ src/backend/packages/inensus/demo-meter-manufacturer/src/Models/DemoMeterTransaction.php
@@ -3,7 +3,13 @@
 namespace Inensus\DemoMeterManufacturer\Models;
 
 use App\Models\Transaction\BaseManufacturerTransaction;
+use Carbon\Carbon;
 
+/**
+ * @property int    $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class DemoMeterTransaction extends BaseManufacturerTransaction {
     protected $table = 'demo_meter_transactions';
 }
--- src/backend/packages/inensus/demo-shs-manufacturer/src/Models/DemoShsTransaction.php
+++ src/backend/packages/inensus/demo-shs-manufacturer/src/Models/DemoShsTransaction.php
@@ -3,7 +3,13 @@
 namespace Inensus\DemoShsManufacturer\Models;
 
 use App\Models\Transaction\BaseManufacturerTransaction;
+use Carbon\Carbon;
 
+/**
+ * @property int    $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class DemoShsTransaction extends BaseManufacturerTransaction {
     protected $table = 'demo_shs_transactions';
 }
