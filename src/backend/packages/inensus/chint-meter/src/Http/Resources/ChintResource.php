996541eba6d96d31cdd6266cd52623cdc330695f
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-17 11:39:06 +0100

    Apply Larastan Level 6 to `chint`

--- src/backend/packages/inensus/chint-meter/src/Http/Resources/ChintResource.php
+++ src/backend/packages/inensus/chint-meter/src/Http/Resources/ChintResource.php
@@ -5,13 +5,4 @@ namespace Inensus\ChintMeter\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ChintResource extends JsonResource {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array
-     */
-    public function toArray(Request $request) {
-        return parent::toArray($request);
-    }
-}
+class ChintResource extends JsonResource {}
