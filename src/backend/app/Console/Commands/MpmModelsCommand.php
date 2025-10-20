4f1fa22934879e0efbf20c92bc81ebfbbd00bc29
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-20 16:59:17 +0100

    Add `ide-helper`

---
+++ src/backend/app/Console/Commands/MpmModelsCommand.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Barryvdh\LaravelIdeHelper\Console\ModelsCommand;
+use Barryvdh\LaravelIdeHelper\Generator;
+use Barryvdh\Reflection\DocBlock;
+use Barryvdh\Reflection\DocBlock\Context;
+use Barryvdh\Reflection\DocBlock\Serializer as DocBlockSerializer;
+use Barryvdh\Reflection\DocBlock\Tag;
+use Illuminate\Support\Str;
+use ReflectionClass;
+
+/**
+ * This file is a copy of the great laravel-ide-helper.
+ * https://github.com/barryvdh/laravel-ide-helper/blob/9ef25f60e70ced86f687ef6b9ffd9ac74a7c388a/src/Console/ModelsCommand.php#L1164.
+ *
+ * Unfortunately laravel-ide-helper lacks some configuration options.
+ * It generates a lot of PHPDocs which are no longer required by Laravel 11+.
+ * And might be outdated and cause confusion.
+ *
+ * What we mainly want is the auto-generation of @property and @property-read
+ * magic methods to be consistent with out database.
+ *
+ * Hence we have customised below like so
+ *
+ * - Do not generate the Eloquent helper file
+ * - Do not generate generic @mixin on models
+ */
+class MpmModelsCommand extends ModelsCommand {
+    // protected $signature = 'mpm:ide-helper';
+    protected $name = 'mpm-ide-helper:models';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle() {
+        $this->filename = $this->laravel['config']->get('ide-helper.models_filename', '_ide_helper_models.php');
+        $filename = $this->option('filename') ?? $this->filename;
+        $this->write = $this->option('write');
+        $this->write_mixin = $this->option('write-mixin');
+        $this->dirs = array_merge(
+            $this->laravel['config']->get('ide-helper.model_locations', []),
+            $this->option('dir')
+        );
+        $model = $this->argument('model');
+        $ignore = $this->option('ignore');
+        $this->reset = $this->option('reset');
+        $this->phpstorm_noinspections = $this->option('phpstorm-noinspections');
+        $this->write_model_magic_where = $this->laravel['config']->get('ide-helper.write_model_magic_where', true);
+        $this->write_model_external_builder_methods = $this->laravel['config']->get('ide-helper.write_model_external_builder_methods', true);
+        $this->write_model_relation_count_properties =
+            $this->laravel['config']->get('ide-helper.write_model_relation_count_properties', true);
+        $this->write_model_relation_exists_properties =
+            $this->laravel['config']->get('ide-helper.write_model_relation_exists_properties', false);
+
+        $this->write = $this->write_mixin ? true : $this->write;
+        // If filename is default and Write is not specified, ask what to do
+        if (!$this->write && $filename === $this->filename && !$this->option('nowrite')) {
+            if (
+                $this->confirm(
+                    "Do you want to overwrite the existing model files? Choose no to write to $filename instead"
+                )
+            ) {
+                $this->write = true;
+            }
+        }
+
+        $this->dateClass = class_exists(\Illuminate\Support\Facades\Date::class)
+            ? '\\'.get_class(\Illuminate\Support\Facades\Date::now())
+            : '\Illuminate\Support\Carbon';
+
+        $content = $this->generateDocs($model, $ignore);
+
+        if (!$this->write || $this->write_mixin) {
+            $written = $this->files->put($filename, $content);
+            if ($written !== false) {
+                $this->info("Model information was written to $filename");
+            } else {
+                $this->error("Failed to write model information to $filename");
+            }
+        }
+
+        $helperFilename = $this->config->get('ide-helper.filename');
+        $writeHelper = $this->option('write-eloquent-helper');
+
+        if (!$writeHelper && !$this->files->exists($helperFilename) && ($this->write || $this->write_mixin)) {
+            if ($this->confirm("{$helperFilename} does not exist.
+            Do you want to generate a minimal helper to generate the Eloquent methods?")) {
+                $writeHelper = true;
+            }
+        }
+
+        if ($writeHelper) {
+            $generator = new Generator($this->config, $this->view, $this->getOutput());
+            $content = $generator->generateEloquent();
+            $written = $this->files->put($helperFilename, $content);
+            if ($written !== false) {
+                $this->info("Eloquent helper was written to $helperFilename");
+            } else {
+                $this->error("Failed to write eloquent helper to $helperFilename");
+            }
+        }
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return string
+     */
+    protected function createPhpDocs($class) {
+        $reflection = new \ReflectionClass($class);
+        $namespace = $reflection->getNamespaceName();
+        $classname = $reflection->getShortName();
+        $originalDoc = $reflection->getDocComment();
+        $keyword = $this->getClassKeyword($reflection);
+        $interfaceNames = array_diff_key(
+            $reflection->getInterfaceNames(),
+            $reflection->getParentClass()->getInterfaceNames()
+        );
+
+        $phpdoc = new DocBlock($reflection, new Context($namespace));
+        if ($this->reset) {
+            $phpdoc->setText(
+                (new DocBlock($reflection, new Context($namespace)))->getText()
+            );
+            foreach ($phpdoc->getTags() as $tag) {
+                if (
+                    in_array($tag->getName(), ['property', 'property-read', 'property-write', 'method', 'mixin'])
+                    || ($tag->getName() === 'noinspection' && in_array($tag->getContent(), ['PhpUnnecessaryFullyQualifiedNameInspection', 'PhpFullyQualifiedNameUsageInspection']))
+                ) {
+                    $phpdoc->deleteTag($tag);
+                }
+            }
+        }
+
+        $properties = [];
+        $methods = [];
+        foreach ($phpdoc->getTags() as $tag) {
+            $name = $tag->getName();
+            if ($name == 'property' || $name == 'property-read' || $name == 'property-write') {
+                $properties[] = $tag->getVariableName();
+            } elseif ($name == 'method') {
+                $methods[] = $tag->getMethodName();
+            }
+        }
+
+        foreach ($this->properties as $name => $property) {
+            $name = "\$$name";
+
+            if ($this->hasCamelCaseModelProperties()) {
+                $name = Str::camel($name);
+            }
+
+            if (in_array($name, $properties)) {
+                continue;
+            }
+            if ($property['read'] && $property['write']) {
+                $attr = 'property';
+            } elseif ($property['write']) {
+                $attr = 'property-write';
+            } else {
+                $attr = 'property-read';
+            }
+
+            $tagLine = trim("@{$attr} {$property['type']} {$name} {$property['comment']}");
+            $tag = Tag::createInstance($tagLine, $phpdoc);
+            $phpdoc->appendTag($tag);
+        }
+
+        ksort($this->methods);
+
+        foreach ($this->methods as $name => $method) {
+            if (in_array($name, $methods)) {
+                continue;
+            }
+            $arguments = implode(', ', $method['arguments']);
+            $tagLine = "@method static {$method['type']} {$name}({$arguments})";
+            if ($method['comment'] !== '') {
+                $tagLine .= " {$method['comment']}";
+            }
+            $tag = Tag::createInstance($tagLine, $phpdoc);
+            $phpdoc->appendTag($tag);
+        }
+
+        if ($this->write) {
+            $eloquentClassNameInModel = $this->getClassNameInDestinationFile($reflection, 'Eloquent');
+
+            // remove the already existing tag to prevent duplicates
+            foreach ($phpdoc->getTagsByName('mixin') as $tag) {
+                if ($tag->getContent() === $eloquentClassNameInModel) {
+                    $phpdoc->deleteTag($tag);
+                }
+            }
+
+            $phpdoc->appendTag(Tag::createInstance('@mixin '.$eloquentClassNameInModel, $phpdoc));
+        }
+
+        if ($this->phpstorm_noinspections) {
+            /*
+             * Facades, Eloquent API
+             * @see https://www.jetbrains.com/help/phpstorm/php-fully-qualified-name-usage.html
+             */
+            $phpdoc->appendTag(Tag::createInstance('@noinspection PhpFullyQualifiedNameUsageInspection', $phpdoc));
+            /*
+             * Relations, other models in the same namespace
+             * @see https://www.jetbrains.com/help/phpstorm/php-unnecessary-fully-qualified-name.html
+             */
+            $phpdoc->appendTag(
+                Tag::createInstance('@noinspection PhpUnnecessaryFullyQualifiedNameInspection', $phpdoc)
+            );
+        }
+
+        $serializer = new DocBlockSerializer();
+        $docComment = $serializer->getDocComment($phpdoc);
+
+        if ($this->write_mixin) {
+            $phpdocMixin = new DocBlock($reflection, new Context($namespace));
+            // remove all mixin tags prefixed with IdeHelper
+            foreach ($phpdocMixin->getTagsByName('mixin') as $tag) {
+                if (Str::startsWith($tag->getContent(), 'IdeHelper')) {
+                    $phpdocMixin->deleteTag($tag);
+                }
+            }
+
+            $mixinClassName = "IdeHelper{$classname}";
+            $phpdocMixin->appendTag(Tag::createInstance("@mixin {$mixinClassName}", $phpdocMixin));
+            $mixinDocComment = $serializer->getDocComment($phpdocMixin);
+            // remove blank lines if there's no text
+            if (!$phpdocMixin->getText()) {
+                $mixinDocComment = preg_replace("/\s\*\s*\n/", '', $mixinDocComment);
+            }
+
+            foreach ($phpdoc->getTagsByName('mixin') as $tag) {
+                if (Str::startsWith($tag->getContent(), 'IdeHelper')) {
+                    $phpdoc->deleteTag($tag);
+                }
+            }
+            $docComment = $serializer->getDocComment($phpdoc);
+        }
+
+        if ($this->write) {
+            $modelDocComment = $this->write_mixin ? $mixinDocComment : $docComment;
+            $filename = $reflection->getFileName();
+            $contents = $this->files->get($filename);
+            if ($originalDoc) {
+                $contents = str_replace($originalDoc, $modelDocComment, $contents);
+            } else {
+                $replace = "{$modelDocComment}\n";
+                $pos = strpos($contents, "final class {$classname}") ?: strpos($contents, "class {$classname}");
+                if ($pos !== false) {
+                    $contents = substr_replace($contents, $replace, $pos, 0);
+                }
+            }
+            if ($this->files->put($filename, $contents)) {
+                $this->info('Written new phpDocBlock to '.$filename);
+            }
+        }
+
+        $classname = $this->write_mixin ? $mixinClassName : $classname;
+
+        $allowDynamicAttributes = $this->write_mixin ? "#[\AllowDynamicProperties]\n\t" : '';
+        $output = "namespace {$namespace}{\n{$docComment}\n\t{$allowDynamicAttributes}{$keyword}class {$classname} ";
+
+        if (!$this->write_mixin) {
+            $output .= "extends \Eloquent ";
+
+            if ($interfaceNames) {
+                $interfaces = implode(', \\', $interfaceNames);
+                $output .= "implements \\{$interfaces} ";
+            }
+        }
+
+        return $output."{}\n}\n\n";
+    }
+}
