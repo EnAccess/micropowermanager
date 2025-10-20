bb6958fb6cb16f302c998d979f79255c78341321
Dibyendu Sahoo <dibyendusahoo03@gmail.com>
2025-10-20 09:16:08 +0000

    Removed unused function

--- src/backend/app/Models/Base/BaseModel.php
+++ src/backend/app/Models/Base/BaseModel.php
@@ -25,9 +25,4 @@ abstract class BaseModel extends Model {
 
         parent::__construct($attributes);
     }
-
-    public function resolveChildRouteBinding($childType, $value, $field) {
-        // TODO: Implement resolveChildRouteBinding() method.
-        throw new \Exception('Method resolveChildRouteBinding() not yet implemented.');
-    }
 }
