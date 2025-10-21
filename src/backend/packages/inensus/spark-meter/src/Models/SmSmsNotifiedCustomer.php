84b1c6eeb4e105a81af647bf6247973fa24c3ae7
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-21 16:19:23 +0100

    Fix Copy/Paste

--- src/backend/packages/inensus/spark-meter/src/Models/SmSmsNotifiedCustomer.php
+++ src/backend/packages/inensus/spark-meter/src/Models/SmSmsNotifiedCustomer.php
@@ -8,17 +8,20 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Carbon;
 
 /**
- * @property      int                  $id
- * @property      string               $customer_id
- * @property      string               $notify_type
- * @property      string|null          $notify_id
- * @property      Carbon|null          $created_at
- * @property      Carbon|null          $updated_at
- * @property-read Model|\Eloquent|null $notify
+ * @property      int         $id
+ * @property      string      $customer_id
+ * @property      string      $notify_type
+ * @property      string|null $notify_id
+ * @property      Carbon|null $created_at
+ * @property      Carbon|null $updated_at
+ * @property-read Model|null  $notify
  */
 class SmSmsNotifiedCustomer extends BaseModel {
     protected $table = 'sm_sms_notified_customers';
 
+    /**
+     * @return MorphTo<Model, $this>
+     */
     public function notify(): MorphTo {
         return $this->morphTo();
     }
