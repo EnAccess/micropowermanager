7377efeaad16dbd71f4a52f160c27a781f9320b0
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-21 09:50:49 +0100

    Update PHP CS Fixer to allow PHPDocs for `return` statement

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -40,6 +40,9 @@ return (new PhpCsFixer\Config())
         'phpdoc_align' => [
             'tags' => ['method', 'param', 'property', 'property-read', 'return', 'throws', 'type', 'var'],
         ],
+        'phpdoc_to_comment' => [
+            'allow_before_return_statement' => true,
+        ],
         // disabled for editor integration
         'no_unused_imports' => false,
     ])
