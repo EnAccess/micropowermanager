574f115de238ce3a29f59ef70dfbc5b8829aec96
Daniel Mohns <daniel.mohns@posteo.de>
2025-10-21 16:19:23 +0100

    Fix Copy/Paste

--- src/backend/packages/inensus/spark-meter/src/Models/SmSetting.php
+++ src/backend/packages/inensus/spark-meter/src/Models/SmSetting.php
@@ -8,16 +8,19 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Carbon;
 
 /**
- * @property      int             $id
- * @property      int             $setting_id
- * @property      string          $setting_type
- * @property      Carbon|null     $created_at
- * @property      Carbon|null     $updated_at
- * @property-read Model|\Eloquent $setting
+ * @property      int         $id
+ * @property      int         $setting_id
+ * @property      string      $setting_type
+ * @property      Carbon|null $created_at
+ * @property      Carbon|null $updated_at
+ * @property-read Model       $setting
  */
 class SmSetting extends BaseModel {
     protected $table = 'sm_settings';
 
+    /**
+     * @return MorphTo<Model, $this>
+     */
     public function setting(): MorphTo {
         return $this->morphTo();
     }
