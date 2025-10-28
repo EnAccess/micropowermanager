a7add6cfe052f0c9c7383b4b88c2e339d76458e1
alaincodes24 <alaincodes24@gmail.com>
2025-10-28 15:38:27 +0200

    Update scribe config following v5 upgrade

--- src/backend/config/scribe.php
+++ src/backend/config/scribe.php
@@ -5,6 +5,7 @@ use Knuckles\Scribe\Extracting\Strategies\BodyParameters\GetFromBodyParamAttribu
 use Knuckles\Scribe\Extracting\Strategies\BodyParameters\GetFromBodyParamTag;
 use Knuckles\Scribe\Extracting\Strategies\Headers\GetFromHeaderAttribute;
 use Knuckles\Scribe\Extracting\Strategies\Headers\GetFromHeaderTag;
+use Knuckles\Scribe\Extracting\Strategies\StaticData;
 use Knuckles\Scribe\Extracting\Strategies\Metadata\GetFromDocBlocks;
 use Knuckles\Scribe\Extracting\Strategies\Metadata\GetFromMetadataAttributes;
 use Knuckles\Scribe\Extracting\Strategies\QueryParameters\GetFromFormRequest;
@@ -59,9 +60,6 @@ DESC,
 
                 // Match only routes whose domains match this pattern (use * as a wildcard to match any characters). Example: 'api.*'.
                 'domains' => ['*'],
-
-                // [Dingo router only] Match only routes registered under this version. Wildcards are NOT supported.
-                'versions' => ['v1'],
             ],
 
             // Include these routes even if they did not match the rules above.
@@ -256,11 +254,13 @@ INTRO,
             GetFromHeaderAttribute::class,
             GetFromHeaderTag::class,
             [
-                'override',
+                StaticData::class,
                 [
-                    'Content-Type' => 'application/json',
-                    'Accept' => 'application/json',
-                ],
+                    'data' => [
+                        'Content-Type' => 'application/json',
+                        'Accept' => 'application/json',
+                    ]
+                ]
             ],
         ],
         'bodyParameters' => [
