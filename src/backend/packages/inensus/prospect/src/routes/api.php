77af9b60e8cc9afe2b4c2dcfb4c028534f5fd4d3
alaincodes24 <alaincodes24@gmail.com>
2025-11-04 15:08:15 +0200

    Use controller class references in routes

--- src/backend/packages/inensus/prospect/src/routes/api.php
+++ src/backend/packages/inensus/prospect/src/routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inensus\Prospect\Http\Controllers\ProspectCredentialController;
+use Inensus\Prospect\Http\Controllers\ProspectSyncSettingController;
 
 Route::group(['prefix' => 'prospect'], function () {
     Route::group(['prefix' => 'credential'], function () {
-        Route::get('/', 'ProspectCredentialController@show');
-        Route::put('/', 'ProspectCredentialController@update');
+        Route::get('/',
+        [ProspectCredentialController::class, 'show'] );
+        Route::put('/', [ProspectCredentialController::class, 'update']);
     });
     Route::group(['prefix' => 'prospect-setting'], function () {
         Route::group(['prefix' => 'sync-setting'], function () {
-            Route::put('/', 'ProspectSyncSettingController@update');
+            Route::put('/',
+            [ProspectSyncSettingController::class, 'update']);
         });
     });
 });
