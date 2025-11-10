0c093a04599728776bb5b28a525124c9922886f7
Obinna Ikeh <hikenike6@gmail.com>
2025-11-10 13:16:26 +0100

    Remove tail tag from demo manufacturer plugins

---
+++ src/backend/database/migrations/2025_11_10_112521_rename_tail_tag_demo_meter_manufacturer_mpm_plugin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\MpmPlugin;
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        DB::table('mpm_plugins')->where('id', MpmPlugin::DEMO_METER_MANUFACTURER)->update([
+            'tail_tag' => null,
+        ]);
+        DB::table('mpm_plugins')->where('id', MpmPlugin::DEMO_SHS_MANUFACTURER)->update([
+            'tail_tag' => null,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        DB::table('mpm_plugins')->where('id', MpmPlugin::DEMO_METER_MANUFACTURER)->update([
+            'tail_tag' => 'DemoMeterManufacturer',
+        ]);
+        DB::table('mpm_plugins')->where('id', MpmPlugin::DEMO_SHS_MANUFACTURER)->update([
+            'tail_tag' => 'DemoShsManufacturer',
+        ]);
+    }
+};
