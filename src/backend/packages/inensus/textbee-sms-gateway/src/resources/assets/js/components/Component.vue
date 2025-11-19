02991c9530d0a1d7807a7f6f8d8bd369032418e3
Obinna Ikeh <hikenike6@gmail.com>
2025-11-19 14:47:44 +0100

    Implement SMS gateway plugin with TextBee

---
+++ src/backend/packages/inensus/textbee-sms-gateway/src/resources/assets/js/components/Component.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+<script>
+  export default {
+    name: 'Component',
+    data () {
+      return {}
+    },
+    mounted () {
+
+    },
+    beforeDestroy () {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
