a24d4f16fb1d2bd9fe2401a6d8acf560fdf05b39
Obinna Ikeh <hikenike6@gmail.com>
2025-11-21 00:48:04 +0100

    Remove unused src/resources from textbee plugin

--- src/backend/packages/inensus/textbee-sms-gateway/src/resources/assets/js/routes.js
+++
@@ -1,7 +0,0 @@
-let routes = [
-  {
-    path: '/{{menu-item}}/{{submenu-item}}/page/:page_number',
-    component: require('./plugins/textbee-sms-gateway/js/components/Component').default,
-    meta: { layout: 'default' },
-  },
-]
