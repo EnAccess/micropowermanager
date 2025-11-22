df4779a50aae64f191f7c6294d4b63ec40c238f0
alaincodes24 <alaincodes24@gmail.com>
2025-11-22 12:04:27 +0200

    Create and update payments sync settings

--- src/backend/packages/inensus/prospect/src/Http/Controllers/ProspectSyncSettingController.php
+++ src/backend/packages/inensus/prospect/src/Http/Controllers/ProspectSyncSettingController.php
@@ -4,17 +4,18 @@ namespace Inensus\Prospect\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Inensus\Prospect\Http\Resources\ProspectResource;
+use Inensus\Prospect\Http\Resources\ProspectSyncSettingResource;
 use Inensus\Prospect\Services\ProspectSyncSettingService;
 
 class ProspectSyncSettingController extends Controller {
     public function __construct(private ProspectSyncSettingService $syncSettingService) {}
 
-    public function index(): ProspectResource {
-        return new ProspectResource($this->syncSettingService->getSyncSettings());
+    public function index() {
+        return ProspectSyncSettingResource::collection($this->syncSettingService->getSyncSettings());
     }
 
-    public function update(Request $request): ProspectResource {
-        return new ProspectResource($this->syncSettingService->updateSyncSettings($request->all()));
+    public function update(Request $request) {
+        $payload = $request->json()->all() ?? $request->all();
+        return ProspectSyncSettingResource::collection($this->syncSettingService->updateSyncSettings($payload));
     }
 }
---
+++ src/backend/packages/inensus/prospect/src/Http/Resources/ProspectSyncSettingResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Inensus\Prospect\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProspectSyncSettingResource extends JsonResource {
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array {
+        return [
+            'data' => [
+                'type' => 'sync_setting',
+                'id' => $this->id,
+                'attributes' => [
+                    'id' => $this->id,
+                    'actionName' => $this->action_name,
+                    'syncInValueStr' => $this->sync_in_value_str,
+                    'syncInValueNum' => $this->sync_in_value_num,
+                    'maxAttempts' => $this->max_attempts,
+                ],
+            ],
+        ];
+    }
+}
