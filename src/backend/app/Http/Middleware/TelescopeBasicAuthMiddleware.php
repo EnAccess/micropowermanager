64953646f1380b83bf85315c321333d46be2fa1d
Obinna Ikeh <hikenike6@gmail.com>
2025-12-18 11:26:29 +0100

    setup telescope on mpm

---
+++ src/backend/app/Http/Middleware/TelescopeBasicAuthMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+
+class TelescopeBasicAuthMiddleware {
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request  $request
+     * @param \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, \Closure $next) {
+        if (app()->environment('development')) {
+            return $next($request);
+        }
+
+        $username = config('telescope.http_basic_auth.username');
+        $password = config('telescope.http_basic_auth.password');
+
+        if (empty($username) || empty($password)) {
+            abort(403, 'Telescope access not configured.');
+        }
+
+        // Check HTTP Basic Auth
+        if ($request->getUser() !== $username || $request->getPassword() !== $password) {
+            return response('Unauthorized', 401, [
+                'WWW-Authenticate' => 'Basic realm="Telescope"',
+            ]);
+        }
+
+        return $next($request);
+    }
+}
