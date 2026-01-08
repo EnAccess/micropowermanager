2b691f48aa95aa7fffdfb23db75b59b5a5813be5
Beesafe <hikenike6@gmail.com>
2026-01-08 15:51:18 +0100

    setup telescope on mpm (#1220)
    * setup telescope on mpm
    
    * Fix spacing issues
    
    * Apply rector for codebase quality
    
    * Fix php-cs-fixer error
    
    * Hardcord db connection to run telesccope migrations
    
    * minor refactor:  remove telescope from providers config
    
    This commit enables telescope by default on dev environment and removes reference to TelescopeServiceProvider from the list of providers in `boostrap/providers.php` because we are already registering it in AppServiceProvider.
    
    * allow configuration telescope data retention time

--- src/backend/app/Providers/AppServiceProvider.php
+++ src/backend/app/Providers/AppServiceProvider.php
@@ -129,5 +129,11 @@ class AppServiceProvider extends ServiceProvider {
             TransactionSuccessfulEvent::class,
             TransactionSuccessfulListener::class
         );
+
+        // Register TelescopeServiceProvider
+        if (config('telescope.enabled', false)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
