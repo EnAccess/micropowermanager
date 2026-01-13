e98a440389b6a780c2ff132175d55bae0b088f4c
Daniel Mohns <daniel.mohns@posteo.de>
2026-01-13 14:35:06 +0100

    Remove unneeded `js` files

--- src/plugin-template/frontend/routes.js
+++
@@ -1,8 +0,0 @@
-let routes = [
-  {
-    path: "/{{menu-item}}/{{submenu-item}}/page/:page_number",
-    component: require("./plugins/{{plugin-name}}/js/components/Component")
-      .default,
-    meta: { layout: "default" },
-  },
-];
