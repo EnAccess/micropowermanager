d3168af5aa7137a87cd0dc2de03226853c26e9ab
Daniel Mohns <daniel.mohns@posteo.de>
2026-02-23 12:32:18 +0100

    Simplify `Village` relations

---
+++ src/backend/database/migrations/tenant/2026_02_23_111733_remove_cluster_id_column_from_cities.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::connection('tenant')->table('cities', function (Blueprint $table) {
+            $table->dropColumn('cluster_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::connection('tenant')->table('cities', function (Blueprint $table) {
+            $table->integer('cluster_id')->after('country_id');
+        });
+
+        // Repopulate cluster_id from city -> minigrid -> cluster
+        City::with('miniGrid')->chunkById(100, function ($cities) {
+            foreach ($cities as $city) {
+                if ($city->miniGrid && $city->miniGrid->cluster_id) {
+                    DB::table('cities')
+                        ->where('id', $city->id)
+                        ->update([
+                            'cluster_id' => $city->miniGrid->cluster_id,
+                        ]);
+                }
+            }
+        });
+    }
+};
