0209b1dfb18c40383b5380434f85d1557b65f349
Obinna Ikeh <hikenike6@gmail.com>
2026-05-18 19:35:20 +0100

    Apply prettier fixes

--- src/frontend/src/modules/Agent/AgentDetail.vue
+++ src/frontend/src/modules/Agent/AgentDetail.vue
@@ -7,212 +7,221 @@
       button-icon="delete"
       @widgetAction="confirmDelete"
     >
-    <md-card>
-      <md-card-content>
-        <div class="md-layout md-gutter md-alignment-center" v-if="!editAgent">
-          <div class="md-layout-item">
-            <div class="md-layout md-alignment-center-left">
-              <div class="md-layout-item md-size-20">
-                <md-avatar class="md-avatar-icon md-large">
-                  {{ initials }}
-                </md-avatar>
+      <md-card>
+        <md-card-content>
+          <div
+            class="md-layout md-gutter md-alignment-center"
+            v-if="!editAgent"
+          >
+            <div class="md-layout-item">
+              <div class="md-layout md-alignment-center-left">
+                <div class="md-layout-item md-size-20">
+                  <md-avatar class="md-avatar-icon md-large">
+                    {{ initials }}
+                  </md-avatar>
+                </div>
+                <div class="md-layout-item">
+                  <h2>
+                    {{ agent.name }}
+                    {{ agent.surname }}
+                  </h2>
+                </div>
               </div>
-              <div class="md-layout-item">
-                <h2>
-                  {{ agent.name }}
-                  {{ agent.surname }}
-                </h2>
-              </div>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+              <md-button
+                @click="editAgent = true"
+                class="md-icon-button"
+                style="float: right"
+              >
+                <md-icon>create</md-icon>
+              </md-button>
+            </div>
+
+            <div class="md-layout-item md-size-100">
+              <md-list class="md-double-line">
+                <md-list-item>
+                  <md-icon>wc</md-icon>
+                  <div class="md-list-item-text">
+                    <span>{{ $tc("words.gender") }}</span>
+                    <span>{{ agent.gender || "N/A" }}</span>
+                  </div>
+                </md-list-item>
+                <md-divider></md-divider>
+                <md-list-item>
+                  <md-icon>phone</md-icon>
+                  <div class="md-list-item-text">
+                    <span>{{ $tc("words.phone") }}</span>
+                    <span>
+                      {{ agent.phone || "N/A" }}
+                    </span>
+                  </div>
+                </md-list-item>
+                <md-divider></md-divider>
+                <md-list-item>
+                  <md-icon>cake</md-icon>
+                  <div class="md-list-item-text">
+                    <span>{{ $tc("words.birthday") }}</span>
+                    <span>
+                      {{ agent.birthday || "N/A" }}
+                    </span>
+                  </div>
+                </md-list-item>
+                <md-divider></md-divider>
+                <md-list-item>
+                  <md-icon>tag</md-icon>
+                  <div class="md-list-item-text">
+                    <span>{{ $tc("words.type") }}</span>
+                    <span>
+                      {{ agent.commissionType || "N/A" }}
+                    </span>
+                  </div>
+                </md-list-item>
+                <md-divider></md-divider>
+                <md-list-item>
+                  <md-icon>account_balance_wallet</md-icon>
+                  <div class="md-list-item-text">
+                    <span>{{ $tc("words.balance") }}</span>
+                    <span>
+                      {{ formatCurrency(agent.balance || 0) }}
+                    </span>
+                  </div>
+                </md-list-item>
+              </md-list>
             </div>
           </div>
 
-          <div class="md-layout-item md-size-20">
-            <md-button
-              @click="editAgent = true"
-              class="md-icon-button"
-              style="float: right"
-            >
-              <md-icon>create</md-icon>
-            </md-button>
-          </div>
+          <div class="md-layout md-gutter" v-else>
+            <div class="md-layout-item md-size-100">
+              <form novalidate class="md-layout" @submit.prevent="updateAgent">
+                <md-card class="md-layout-item md-size-100">
+                  <md-card-content>
+                    <md-field>
+                      <label for="name">
+                        {{ $tc("words.name") }}
+                      </label>
+                      <md-input
+                        type="text"
+                        name="name"
+                        id="name"
+                        v-model="agent.name"
+                      />
+                    </md-field>
 
-          <div class="md-layout-item md-size-100">
-            <md-list class="md-double-line">
-              <md-list-item>
-                <md-icon>wc</md-icon>
-                <div class="md-list-item-text">
-                  <span>{{ $tc("words.gender") }}</span>
-                  <span>{{ agent.gender || "N/A" }}</span>
-                </div>
-              </md-list-item>
-              <md-divider></md-divider>
-              <md-list-item>
-                <md-icon>phone</md-icon>
-                <div class="md-list-item-text">
-                  <span>{{ $tc("words.phone") }}</span>
-                  <span>
-                    {{ agent.phone || "N/A" }}
-                  </span>
-                </div>
-              </md-list-item>
-              <md-divider></md-divider>
-              <md-list-item>
-                <md-icon>cake</md-icon>
-                <div class="md-list-item-text">
-                  <span>{{ $tc("words.birthday") }}</span>
-                  <span>
-                    {{ agent.birthday || "N/A" }}
-                  </span>
-                </div>
-              </md-list-item>
-              <md-divider></md-divider>
-              <md-list-item>
-                <md-icon>tag</md-icon>
-                <div class="md-list-item-text">
-                  <span>{{ $tc("words.type") }}</span>
-                  <span>
-                    {{ agent.commissionType || "N/A" }}
-                  </span>
-                </div>
-              </md-list-item>
-              <md-divider></md-divider>
-              <md-list-item>
-                <md-icon>account_balance_wallet</md-icon>
-                <div class="md-list-item-text">
-                  <span>{{ $tc("words.balance") }}</span>
-                  <span>
-                    {{ formatCurrency(agent.balance || 0) }}
-                  </span>
-                </div>
-              </md-list-item>
-            </md-list>
-          </div>
-        </div>
+                    <md-field>
+                      <label for="surname">
+                        {{ $tc("words.surname") }}
+                      </label>
+                      <md-input
+                        type="text"
+                        name="surname"
+                        id="surname"
+                        v-model="agent.surname"
+                      />
+                    </md-field>
 
-        <div class="md-layout md-gutter" v-else>
-          <div class="md-layout-item md-size-100">
-            <form novalidate class="md-layout" @submit.prevent="updateAgent">
-              <md-card class="md-layout-item md-size-100">
-                <md-card-content>
-                  <md-field>
-                    <label for="name">
-                      {{ $tc("words.name") }}
-                    </label>
-                    <md-input
-                      type="text"
-                      name="name"
-                      id="name"
-                      v-model="agent.name"
-                    />
-                  </md-field>
-
-                  <md-field>
-                    <label for="surname">
-                      {{ $tc("words.surname") }}
-                    </label>
-                    <md-input
-                      type="text"
-                      name="surname"
-                      id="surname"
-                      v-model="agent.surname"
-                    />
-                  </md-field>
-
-                  <md-datepicker
-                    name="birthDate"
-                    md-immediately
-                    v-model="agent.birthday"
-                    :md-close-on-blur="false"
-                  >
-                    <label for="birth-date">Birthday :</label>
-                  </md-datepicker>
-
-                  <md-field>
-                    <label for="gender">{{ $tc("words.gender") }} :</label>
-                    <md-select name="gender" id="gender" v-model="agent.gender">
-                      <md-option disabled v-if="agent.gender == null">
-                        -- Select --
-                      </md-option>
-                      <md-option value="male">
-                        {{ $tc("words.male") }}
-                      </md-option>
-                      <md-option value=" female">
-                        {{ $tc("words.female") }}
-                      </md-option>
-                    </md-select>
-                  </md-field>
-
-                  <md-field v-if="$can('settings')">
-                    <label for="commission">
-                      {{ $tc("phrases.commissionType") }}:
-                    </label>
-                    <md-select
-                      name="commission"
-                      id="commission"
-                      v-model="agent.commissionTypeId"
+                    <md-datepicker
+                      name="birthDate"
+                      md-immediately
+                      v-model="agent.birthday"
+                      :md-close-on-blur="false"
                     >
-                      <md-option
-                        v-for="commission in agentCommissions"
-                        :value="commission.id"
-                        :key="commission.id"
-                      >
-                        {{ commission.name }}
-                      </md-option>
-                    </md-select>
-                  </md-field>
+                      <label for="birth-date">Birthday :</label>
+                    </md-datepicker>
 
-                  <md-field>
-                    <label for="miniGrid">{{ $tc("words.miniGrid") }} :</label>
-                    <md-select
-                      name="miniGrid"
-                      id="miniGrid"
-                      v-model="agent.miniGridId"
+                    <md-field>
+                      <label for="gender">{{ $tc("words.gender") }} :</label>
+                      <md-select
+                        name="gender"
+                        id="gender"
+                        v-model="agent.gender"
+                      >
+                        <md-option disabled v-if="agent.gender == null">
+                          -- Select --
+                        </md-option>
+                        <md-option value="male">
+                          {{ $tc("words.male") }}
+                        </md-option>
+                        <md-option value=" female">
+                          {{ $tc("words.female") }}
+                        </md-option>
+                      </md-select>
+                    </md-field>
+
+                    <md-field v-if="$can('settings')">
+                      <label for="commission">
+                        {{ $tc("phrases.commissionType") }}:
+                      </label>
+                      <md-select
+                        name="commission"
+                        id="commission"
+                        v-model="agent.commissionTypeId"
+                      >
+                        <md-option
+                          v-for="commission in agentCommissions"
+                          :value="commission.id"
+                          :key="commission.id"
+                        >
+                          {{ commission.name }}
+                        </md-option>
+                      </md-select>
+                    </md-field>
+
+                    <md-field>
+                      <label for="miniGrid">
+                        {{ $tc("words.miniGrid") }} :
+                      </label>
+                      <md-select
+                        name="miniGrid"
+                        id="miniGrid"
+                        v-model="agent.miniGridId"
+                      >
+                        <md-option
+                          v-for="mg in miniGrids"
+                          :value="mg.id"
+                          :key="mg.id"
+                        >
+                          {{ mg.name }}
+                        </md-option>
+                      </md-select>
+                    </md-field>
+
+                    <md-field>
+                      <label for="phone">
+                        {{ $tc("words.phone") }}
+                      </label>
+                      <md-input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        v-model="agent.phone"
+                      />
+                    </md-field>
+                  </md-card-content>
+                  <md-progress-bar md-mode="indeterminate" v-if="loading" />
+                  <md-card-actions>
+                    <md-button
+                      type="submit"
+                      class="md-primary btn-save"
+                      :disabled="loading"
                     >
-                      <md-option
-                        v-for="mg in miniGrids"
-                        :value="mg.id"
-                        :key="mg.id"
-                      >
-                        {{ mg.name }}
-                      </md-option>
-                    </md-select>
-                  </md-field>
-
-                  <md-field>
-                    <label for="phone">
-                      {{ $tc("words.phone") }}
-                    </label>
-                    <md-input
-                      type="text"
-                      name="phone"
-                      id="phone"
-                      v-model="agent.phone"
-                    />
-                  </md-field>
-                </md-card-content>
-                <md-progress-bar md-mode="indeterminate" v-if="loading" />
-                <md-card-actions>
-                  <md-button
-                    type="submit"
-                    class="md-primary btn-save"
-                    :disabled="loading"
-                  >
-                    {{ $tc("words.save") }}
-                  </md-button>
-                  <md-button
-                    type="button"
-                    @click="editAgent = false"
-                    class="md-accent btn-save"
-                  >
-                    {{ $tc("words.cancel") }}
-                  </md-button>
-                </md-card-actions>
-              </md-card>
-            </form>
+                      {{ $tc("words.save") }}
+                    </md-button>
+                    <md-button
+                      type="button"
+                      @click="editAgent = false"
+                      class="md-accent btn-save"
+                    >
+                      {{ $tc("words.cancel") }}
+                    </md-button>
+                  </md-card-actions>
+                </md-card>
+              </form>
+            </div>
           </div>
-        </div>
-      </md-card-content>
-    </md-card>
+        </md-card-content>
+      </md-card>
     </widget>
 
     <widget
